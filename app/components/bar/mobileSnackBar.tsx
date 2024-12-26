85f633c727104a76f6298fde55058b392cd59ac6
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-12-26 15:48:58 +0900

    feat: Add timer at MobileSnackBar component

--- app/components/bar/mobileSnackBar.tsx
+++ app/components/bar/mobileSnackBar.tsx
@@ -1,16 +1,33 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
 import snackbarIcon from "@/images/icon/snackbarIcon.svg";
 import Image from "next/image";
 
 interface MobileSnackBarProps {
   text: string;
+  duration?: number;
 }
 
 /**
  *
  * @param {string} text snackBar에 표시될 텍스트
+ * @param {number} duration 스낵바 유지 시간 (초 단위 / default 1초)
  * @returns
  */
-const MobileSnackBar = ({ text }: MobileSnackBarProps) => {
+const MobileSnackBar = ({ text, duration = 1 }: MobileSnackBarProps) => {
+  const [visible, setVisible] = useState<boolean>(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setVisible(false);
+    }, duration * 1000);
+
+    return () => clearTimeout(timer);
+  }, [duration]);
+
+  if (!visible) return null;
+
   return (
     <div
       className="w-full fixed bottom-0 left-0 px-[17px] pb-5 md:hidden"
