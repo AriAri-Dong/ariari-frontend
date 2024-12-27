543708d2b2aeee3e07cee480e2bac3061d69ce7b
bsu0404 <bsu0404@naver.com>
2024-12-27 16:06:24 +0900

    Modify application card component

--- app/components/card/applicationCard.tsx
+++ app/components/card/applicationCard.tsx
@@ -1,9 +1,15 @@
 "use client";
 
 import React from "react";
-import Image from "next/image";
 import { useRouter } from "next/navigation";
-import pen from "@/images/icon/pen.svg";
+import Image from "next/image";
+
+import pen from "@/images/icon/application/pen.svg";
+import check from "@/images/icon/application/check.svg";
+import document from "@/images/icon/application/document.svg";
+import tear from "@/images/icon/application/tear.svg";
+import congratulation from "@/images/icon/application/congratulation.svg";
+
 import chevron_right from "@/images/icon/chevron_right.svg";
 import deleteIcon from "@/images/icon/delete.svg";
 
@@ -22,7 +28,7 @@ export interface ApplicationCardProps {
  * @param applicationStatus 나의 동아리 지원 상태
  * @param clubStatus 동아리 모집 진행 상태
  * @param date 작성일
-
+ *
  * @returns
  */
 
@@ -45,25 +51,43 @@ const ApplicationCard = ({
     { id: 3, title: "최종발표 완료" },
   ];
 
+  const getIconForStatus = (status: string) => {
+    switch (status) {
+      case "작성중":
+        return pen;
+      case "서류 심사중":
+        return document;
+      case "서류 합격":
+        return check;
+      case "서류 불합격":
+        return tear;
+      case "최종 합격":
+        return congratulation;
+      case "최종 불합격":
+        return tear;
+    }
+  };
+
   return (
     <div
-      className="w-full flex flex-col gap-3 py-3.5 px-4 mb-3 bg-background rounded-[8px] cursor-pointer md:pl-9 md:pr-[30px] md:py-[26px] md:mb-3.5 md:gap-5"
+      tabIndex={0}
+      className="w-full flex flex-col gap-3 py-3.5 px-4 mb-3 bg-background rounded-[8px] cursor-pointer focus:bg-hover md:hover:bg-hover md:focus:bg-pressed md:pl-9 md:pr-[30px] md:py-[26px] md:mb-3.5 md:gap-5 "
       onClick={onApplicationClick}
     >
       <div className="flex justify-between items-center">
         <div className="flex items-center gap-1 md:gap-1.5">
           <Image
-            src={pen}
-            alt={"pen"}
+            src={getIconForStatus(applicationStatus)}
+            alt={applicationStatus}
             width={18}
             height={18}
             className="md:w-5 md:h-5"
           />
-          <div className="text-right text-subtext1 text-h4_sb">
+          <div className="text-right text-subtext1 text-mobile_body1_sb md:text-h4_sb">
             {applicationStatus}
           </div>
         </div>
-        {applicationStatus == "작성중" && (
+        {applicationStatus === "작성중" && (
           <Image
             src={deleteIcon}
             alt={"delete"}
@@ -79,12 +103,12 @@ const ApplicationCard = ({
                 <div className="flex items-center gap-2">
                   <div
                     className={`w-5 h-5 ${
-                      item.title == clubStatus ? "bg-primary" : "bg-token_bg"
+                      item.title === clubStatus ? "bg-primary" : "bg-token_bg"
                     } rounded-[20px] flex-col justify-center items-center gap-2.5 inline-flex`}
                   >
                     <div
                       className={`text-center text-body4_r ${
-                        item.title == clubStatus
+                        item.title === clubStatus
                           ? "text-background"
                           : "text-subtext2"
                       }`}
@@ -96,14 +120,8 @@ const ApplicationCard = ({
                     {item.title}
                   </div>
                 </div>
-                {item.id != 3 && (
-                  <Image
-                    src={chevron_right}
-                    alt={"2"}
-                    width={20}
-                    height={20}
-                    className="md:w-5 md:h-5 "
-                  />
+                {item.id !== 3 && (
+                  <Image src={chevron_right} alt={"2"} width={20} height={20} />
                 )}
               </div>
             );
@@ -112,10 +130,14 @@ const ApplicationCard = ({
       </div>
       <div className="border-t border-menuborder"></div>
       <div className="items-center md:flex md:justify-between">
-        <div className="text-text1 text-h4_sb mb-3 md:mb-0">{clubName}</div>
+        <div className="text-text1 text-mobile_body1_m mb-3 md:mb-0 md:text-h4_sb ">
+          {clubName}
+        </div>
         <div className="flex items-center gap-8">
-          <div className="text-subtext2 text-body4_r">{date}</div>
-          {applicationStatus == "작성중" && (
+          <div className="text-subtext2 text-mobile_body4_r md:text-body4_r">
+            {date}
+          </div>
+          {applicationStatus === "작성중" && (
             <Image
               src={deleteIcon}
               alt={"delete"}
