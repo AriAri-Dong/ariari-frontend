e2687182b78b8ffbff4ed22b0150a56d15e592ee
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-12-27 09:33:36 +0900

    feat: Update profileSettingModal.tsx

--- app/components/modal/profileSetting/profileSettingModal.tsx
+++ app/components/modal/profileSetting/profileSettingModal.tsx
@@ -1,39 +1,80 @@
 "use client";
 
 import React, { useState } from "react";
+import Image from "next/image";
+import { PROFILE_SETTING } from "@/data/profileSetting";
+import check from "@/images/icon/check.svg";
+import { useProfileContext } from "@/context/profileConetxt";
 
-import { ProfileProvider } from "@/context/profileConetxt";
-import ProfileSetting from "./profileSetting";
-import Step4 from "./step4";
+const Step1 = () => {
+  const { profileData, updateProfileData } = useProfileContext();
+  const [inputValue, setInputValue] = useState<string>(profileData.username);
 
-interface LoginModalProps {
-  onClose: () => void;
-}
-
-const ProfileSettingModal = ({ onClose }: LoginModalProps) => {
-  const [step, setStep] = useState<number>(1);
-
-  const handleNextStep = (nextStep: number) => {
-    setStep(nextStep);
+  const handleProfileClick = (id: number) => {
+    updateProfileData({ selectedProfileId: id });
   };
 
-  const handleClose = () => {
-    setStep(1);
-    onClose();
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setInputValue(value);
+    updateProfileData({ username: value });
   };
+
+  const selectedProfileData = PROFILE_SETTING.find(
+    (item) => item.id === profileData.selectedProfileId
+  );
 
   return (
-    <ProfileProvider>
-      <div className="fixed inset-0 z-50 md:flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm hidden">
-        <div className="absolute inset-0" onClick={handleClose} />
-        {step < 4 ? (
-          <ProfileSetting step={step} onNextStep={handleNextStep} />
-        ) : (
-          <Step4 />
-        )}
+    <>
+      <div className="flex justify-center mb-4 mt-8">
+        <Image
+          src={selectedProfileData?.imageUrl || ""}
+          alt={selectedProfileData?.alias || "프로필 기본 이미지"}
+          width={112}
+          height={112}
+          className="rounded-full bordser border-menuborder p-1"
+        />
       </div>
-    </ProfileProvider>
+      <div className="relative mb-8">
+        <input
+          className="w-full bg-searchbar text-body1_r text-subtext2 py-[13px] px-[22px] rounded-xl 
+            focus:border-blue-500 focus:outline-none"
+          placeholder="사용자 이름"
+          maxLength={9}
+          value={inputValue}
+          onChange={handleChange}
+        />
+        <div className="absolute right-4 top-1/2 -translate-y-1/2 text-unselected text-body1_r">
+          {inputValue.length}/10
+        </div>
+      </div>
+      <div className="grid grid-cols-4 gap-5">
+        {PROFILE_SETTING.map((item) => (
+          <div
+            key={item.id}
+            className="relative flex justify-center items-center w-[76px] h-[76px] cursor-pointer rounded-full"
+            onClick={() => handleProfileClick(item.id)}
+          >
+            <Image
+              src={item.imageUrl}
+              alt={item.alias}
+              width={76}
+              height={76}
+              className="rounded-full"
+            />
+            {profileData.selectedProfileId === item.id && (
+              <>
+                <div className="absolute inset-0 bg-black_50 rounded-full z-10 opacity-70"></div>
+                <div className="absolute z-20">
+                  <Image src={check} alt="check" width={24} height={24} />
+                </div>
+              </>
+            )}
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
-export default ProfileSettingModal;
+export default Step1;
