5de6f74ee765112122cfd0376b8f917a03665754
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-12-27 09:33:21 +0900

    style: Update profileSettingModal.tsx

--- app/components/modal/profileSetting/profileSettingModal.tsx
+++ app/components/modal/profileSetting/profileSettingModal.tsx
@@ -24,7 +24,7 @@ const ProfileSettingModal = ({ onClose }: LoginModalProps) => {
 
   return (
     <ProfileProvider>
-      <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm">
+      <div className="fixed inset-0 z-50 md:flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm hidden">
         <div className="absolute inset-0" onClick={handleClose} />
         {step < 4 ? (
           <ProfileSetting step={step} onNextStep={handleNextStep} />
