74d9775339cd70b7b1db5459ac7637fec1485caf
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-12-30 18:14:12 +0900

    feat: Add notification data

---
+++ app/data/notification.ts
@@ -0,0 +1,15 @@
+import { DataItem } from "@/types/components/notification";
+
+export const TEMP_DATA: DataItem[] = [
+  { title: "Alert", date: "2024.01.19 11:30" },
+  { title: "Club", date: "2024.03.12 11:33" },
+  { title: "Point", date: "2024.03.04 21:29" },
+  { title: "Join", date: "2024.03.04 14:45" },
+  //   { title: "Title", date: "2024.04.01 11:52" },
+  //   { title: "Title", date: "2024.05.23 15:32" },
+  //   { title: "Title", date: "2024.09.09 12:13" },
+  //   { title: "Title", date: "2024.09.29 18:10" },
+  //   { title: "Title", date: "2024.10.27 22:14" },
+  //   { title: "Title", date: "2024.11.12 19:39" },
+  //   { title: "Title", date: "2024.12.16 20:05" },
+];
