420cb42f38f2445a527299279f0f69ab6bdcddbf
yeoniii20 <commetoi_yeoni@naver.com>
2025-01-03 22:08:02 +1100

    feat: Add router

--- app/(club)/club/page.tsx
+++ app/(club)/club/page.tsx
@@ -4,8 +4,16 @@ import React, { useState } from "react";
 import ClubInfoSection from "./content/clubInfoSection";
 import DarkBtn from "@/components/button/withIconBtn/darkBtn";
 import WriteBtn from "@/components/button/iconBtn/writeBtn";
+import { useRouter } from 'next/navigation';
 
 const ClubPage = () => {
+  const router = useRouter()
+
+  const handleRouter = () =>{
+    // 모집 공고 임시 경로
+    router.push("/")
+  }
+
   return (
     <div>
       <ClubInfoSection />
@@ -13,7 +21,7 @@ const ClubPage = () => {
         <WriteBtn onClick={() => {}} />
       </div>
       <div className="fixed bottom-5 left-1/2 transform -translate-x-1/2 md:hidden">
-        <DarkBtn title={"모집공고 보기"} onClick={() => {}} />
+        <DarkBtn title={"모집공고 보기"} onClick={handleRouter} />
       </div>
     </div>
   );
