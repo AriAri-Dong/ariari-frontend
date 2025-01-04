fe7301a068b28d10f884eedac06c2e16cd2776c0
bsu0404 <bsu0404@naver.com>
2025-01-04 20:45:13 +0900

    feat: Add key & modify style

--- app/components/card/withdrawalCard.tsx
+++ app/components/card/withdrawalCard.tsx
@@ -14,7 +14,7 @@ const WithdrawalCard = ({ info }: WithdrawalCardProps) => {
   const router = useRouter();
 
   return (
-    <div className="inline-flex flex-col gap-[14px] md:gap-[18px] ">
+    <div className="flex flex-col gap-[14px] md:gap-[18px] ">
       <div className="text-text1 text-mobile_h3 md:text-h3">{info.title}</div>
       <div className="px-2.5 py-4 md:p-4 bg-hover rounded-12 items-center gap-2 flex">
         <Image
@@ -30,7 +30,10 @@ const WithdrawalCard = ({ info }: WithdrawalCardProps) => {
       </div>
       <div className="flex flex-col gap-2">
         {info.sections.map((item, index) => (
-          <div className="flex gap-1 text-mobile_body1_r md:text-body3_m text-icon">
+          <div
+            key={index}
+            className="flex gap-1 text-mobile_body1_r md:text-body3_m text-icon"
+          >
             <p>{index + 1 + "."}</p>
             <p>{item}</p>
           </div>
