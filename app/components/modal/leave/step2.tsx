6600fa03db1dace6a806cda518683e44afef6e78
bsu0404 <bsu0404@naver.com>
2025-01-04 20:50:08 +0900

    feat: Add select administrator function

--- app/components/modal/leave/step2.tsx
+++ app/components/modal/leave/step2.tsx
@@ -1,51 +1,64 @@
 import React from "react";
-import Image from "next/image";
 
-import not from "@/images/icon/popup/not.svg";
+import SelectAdministrator from "@/components/card/selectAdministrator";
 import LargeBtn from "@/components/button/basicBtn/largeBtn";
-export interface StepProps {
-  handleNextStep: () => void;
-  handleNext2Step: () => void;
-  onClose: () => void;
-  isManager?: boolean;
-}
-const Step2 = ({
-  handleNextStep,
-  handleNext2Step,
-  onClose,
-  isManager = false,
-}: StepProps) => {
-  const managementText =
-    "회원님이 동아리 관리자로 있는 동아리가 있습니다.\n관리자 권한을 다른 사용자에게 위임해주세요.";
-  const commonText = "아리아리 서비스 탈퇴시 일부 기능이 제한됩니다.";
-  return (
-    <div className="w-full flex flex-col justify-center items-center pt-[72px] pb-[26px]">
-      <Image
-        src={not}
-        alt={"logo"}
-        width={134}
-        height={107}
-        className="mb-[46px]"
-      />
-      <p className="text-text1 text-h1_contents_title mb-4">
-        정말 탈퇴하시겠습니까?
-      </p>
-      <p className="text-subtext1 text-h4_r mb-8 text-center whitespace-pre">
-        {isManager ? managementText : commonText}
-      </p>
+import { Delegator, JoinedClub } from "@/types/components/delegate";
 
-      <LargeBtn
-        title={"예"}
-        onClick={isManager ? handleNextStep : handleNext2Step}
-      />
+interface StepProps {
+  clubs: JoinedClub[];
+  selectedDelegates: Record<number, Delegator>;
+  onDelegateSelect: (clubId: number, user: Delegator) => void;
+  handleNextStep: () => void;
+}
+const Step3 = ({
+  clubs,
+  selectedDelegates,
+  onDelegateSelect,
+  handleNextStep,
+}: StepProps) => {
+  const description =
+    "현재 관리자로 운영중인 동아리의 구성원을 직접 선택해\n관리자 권한을 위임할 수 있습니다";
+
+  const handleSubmit = () => {
+    handleNextStep();
+  };
+  return (
+    <div className="hidden md:flex flex-col w-full justify-center pt-9 pb-[26px]">
+      <p className="text-text1 text-h1_contents_title mb-4 text-center">
+        관리자 권한 위임
+      </p>
+      <h3 className="text-center text-subtext1 text-h4_r mb-8 whitespace-pre">
+        {description}
+      </h3>
+      <div className="mb-8">
+        {clubs.map((club, index) => (
+          <div key={club.id}>
+            <SelectAdministrator
+              club={club}
+              selectedUser={
+                selectedDelegates[club.id] || { name: "없음", id: "" }
+              }
+              setSelectedUser={(name: string, id: number) => {
+                onDelegateSelect(club.id, { name, id });
+                console.log(club.name, name, id);
+              }}
+            />
+            {index !== clubs.length - 1 && (
+              <div className="my-2 border-t border-menuborder"></div>
+            )}
+          </div>
+        ))}
+      </div>
+
+      <LargeBtn title={"다음"} onClick={handleSubmit} />
       <button
-        onClick={onClose}
+        onClick={handleNextStep}
         className="block mx-auto mt-4 px-4 py-2.5 text-[13px] font-semibold text-primary md:text-[15px]"
       >
-        아니오
+        건너뛰기
       </button>
     </div>
   );
 };
 
-export default Step2;
+export default Step3;
