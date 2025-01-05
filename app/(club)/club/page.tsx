6b43b464c55ebec2f77457347b3a09a14555f0f7
yeoniii20 <commetoi_yeoni@naver.com>
2025-01-05 23:07:29 +1100

    feat: Add event handler

--- app/(club)/club/page.tsx
+++ app/(club)/club/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 import ClubInfoSection from "./content/clubInfoSection";
 import DarkBtn from "@/components/button/withIconBtn/darkBtn";
 import WriteBtn from "@/components/button/iconBtn/writeBtn";
@@ -14,11 +14,15 @@ const ClubPage = () => {
     router.push("/")
   }
 
+  const handleWrite  = () =>[
+    console.log("작성 핸들러")
+  ]
+
   return (
     <div>
       <ClubInfoSection />
       <div className="fixed bottom-[77px] right-5 md:hidden">
-        <WriteBtn onClick={() => {}} />
+        <WriteBtn onClick={handleWrite} />
       </div>
       <div className="fixed bottom-5 left-1/2 transform -translate-x-1/2 md:hidden">
         <DarkBtn title={"모집공고 보기"} onClick={handleRouter} />
