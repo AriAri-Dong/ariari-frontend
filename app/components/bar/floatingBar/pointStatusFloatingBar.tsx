6a3f83bde1fcefd236f6b099b7f2995a61ca4375
yeoniii20 <commetoi_yeoni@naver.com>
2025-01-07 02:22:20 +1100

    feat: Add props and comment for component

--- app/components/bar/floatingBar/pointStatusFloatingBar.tsx
+++ app/components/bar/floatingBar/pointStatusFloatingBar.tsx
@@ -4,9 +4,19 @@ import React from "react";
 import Image from "next/image";
 import dollar from "@/images/icon/dollar.svg";
 
-interface DayFloatingBarProps {}
+interface DayFloatingBarProps {
+  deductionPoint:number
+  currentPoint: number
+}
 
-const PointStatusFloatingBar = ({}: DayFloatingBarProps) => {
+
+/**
+ * 포인트 현황 플로팅 바 컴포넌트
+ * @param deductionPoint 차감 포인트
+ * @param currentPoint 현재 포인트
+ * @returns 
+ */
+const PointStatusFloatingBar = ({deductionPoint, currentPoint}: DayFloatingBarProps) => {
   return (
     <div
       className="hidden fixed bottom-0 left-0 w-full md:flex justify-center"
@@ -21,11 +31,11 @@ const PointStatusFloatingBar = ({}: DayFloatingBarProps) => {
             <Image src={dollar} alt={"dollar"} width={40} height={40} />
             <h3 className="text-h3 text-primary ml-[21px]">나의 포인트 현황</h3>
             <p className="text-body2_m text-primary ml-[60px]">
-              후기 열람시 5포인트가 차감됩니다.
+              후기 열람시 {deductionPoint}포인트가 차감됩니다.
             </p>
           </div>
           <div className="flex items-center gap-1 text-primary">
-            <div className="text-28">20</div>
+            <div className="text-28">{currentPoint}</div>
             <p className="text-h4_r">p</p>
           </div>
         </div>
