bce84a6d54dfd9fb2e3f13172f5f70a7ff2a04fe
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2025-01-08 17:32:56 +0900

    feat: Add type for badge component

--- app/components/badge/badge.tsx
+++ app/components/badge/badge.tsx
@@ -2,21 +2,53 @@ import React from "react";
 
 interface BadgeProps {
   status: "enable" | "disable";
+  type?: "recruitment" | "answer" | "none";
+  text?: string;
 }
 
-const Badge = ({ status }: BadgeProps) => {
+/**
+ * 뱃지 컴포넌트
+ * @param status 활성/비활성화 상태
+ * @param type 선택 사항, 기본값 "모집"
+ * @param text type이 "none"일 경우 필수
+ * @returns
+ */
+const Badge = ({ status, type = "recruitment", text }: BadgeProps) => {
   const statusVal = status === "enable";
+
+  const getBadgeStyle = () => {
+    if (type === "none") {
+      return statusVal
+        ? "text-primary bg-selectedoption_hover"
+        : "text-subtext2 bg-token_bg";
+    }
+    return statusVal
+      ? "text-primary bg-selectedoption_hover"
+      : "text-subtext2 bg-token_bg";
+  };
+
+  const getBadgeText = () => {
+    if (type === "recruitment") {
+      return statusVal ? "모집중" : "모집마감";
+    }
+    if (type === "answer") {
+      return statusVal ? "답변완료" : "미답변";
+    }
+    if (type === "none") {
+      if (!text) {
+        return "";
+      }
+      return text;
+    }
+    return "";
+  };
+
   return (
     <div className="flex text-center">
       <div
-        className={`rounded text-mobile_body3_m md:text-body3_m
-        py-[3px] w-[62px] md:py-1 md:w-[66px] ${
-          statusVal
-            ? `text-primary bg-selectedoption_hover`
-            : `text-subtext2 bg-token_bg`
-        }`}
+        className={`rounded text-mobile_body3_m md:text-body3_m py-[3px] w-[62px] md:py-1 md:w-[66px] ${getBadgeStyle()}`}
       >
-        {statusVal ? "모집중" : "모집마감"}
+        {getBadgeText()}
       </div>
     </div>
   );
