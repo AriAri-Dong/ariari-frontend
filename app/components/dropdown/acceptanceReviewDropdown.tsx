1357002a250a5754b8e94d0fcf9547e90b1e11ad
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2025-01-10 17:30:37 +0900

    feat: Update acceptanceReviewDropdown.tsx

--- app/components/dropdown/acceptanceReviewDropdown.tsx
+++ app/components/dropdown/acceptanceReviewDropdown.tsx
@@ -1,8 +1,11 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import TransparentSmallBtn from "../button/basicBtn/transparentSmallBtn";
 import IconBadge from "../badge/iconBadge";
+import IconBtn from "../button/withIconBtn/IconBtn";
+import ReportModal from "../modal/reportModal";
+import Alert from "../alert/alert";
 
 interface AcceptanceReviewDropdownProps {
   onClick: () => void;
@@ -21,13 +24,31 @@ const AcceptanceReviewDropdown = ({
   document,
   interview,
 }: AcceptanceReviewDropdownProps) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [inputValue, setInputValue] = useState<string>("");
+  const [isBottomModalOpen, setIsBottomModalOpen] = useState<boolean>(false);
+  const [alertMessage, setAlertMessage] = useState<string | null>(null);
+
+  const handleToggleDropdown = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  const toggleBottomModal = () => {
+    setIsBottomModalOpen((prev) => !prev);
+  };
+
+  const handleReportSubmit = () => {
+    setIsBottomModalOpen(false);
+    setAlertMessage("신고가 정상적으로 접수되었습니다.");
+  };
+
   return (
-    <div
-      className="w-full m-w-[1248px] p-4 md:pt-[18px] md:pb-[22px] md:px-6 rounded-lg
-  bg-background focus:bg-hover md:hover:bg-hover md:focus:bg-pressed cursor-pointer"
-      onClick={onClick}
-    >
-      <div className="flex flex-col items-start gap-3 md:gap-0 md:flex-row md:justify-between md:items-center">
+    <div className="w-full max-w-[1248px] p-4 md:pt-[18px] md:pb-[22px] md:pl-9 md:pr-[34px] rounded-lg bg-background focus:bg-hover md:hover:bg-hover md:focus:bg-pressed cursor-pointer">
+      {/* default */}
+      <div
+        className="flex flex-col items-start gap-3 md:gap-0 md:flex-row md:justify-between md:items-center"
+        onClick={handleToggleDropdown}
+      >
         <div className="flex flex-col items-start md:flex-row gap-3 md:gap-12">
           <div className="flex flex-col gap-4 md:gap-5">
             <h3 className="text-body1_m md:text-h4_sb text-text1">{title}</h3>
@@ -48,6 +69,132 @@ const AcceptanceReviewDropdown = ({
           />
         </div>
       </div>
+
+      {/* 드롭다운 */}
+      <div
+        className={`transition-all duration-300 ease-in-out overflow-hidden ${
+          isOpen ? "max-h-[1000px] opacity-100" : "max-h-0 opacity-0"
+        }`}
+      >
+        <div className="flex w-full justify-between mt-9 mb-2">
+          <div className="flex flex-col w-[183px] gap-[44px]">
+            <div className="flex flex-col gap-[14px]">
+              <h3 className="text-body1_m md:text-h4_sb text-text1">
+                합격전형
+              </h3>
+              <p className=" text-subtext2 text-body2_m">서류 · 면접 전형</p>
+            </div>
+            <div className="flex flex-col gap-[14px]">
+              <h3 className="text-body1_m md:text-h4_sb text-text1">
+                면접방식
+              </h3>
+              <p className=" text-subtext2 text-body2_m">온라인</p>
+            </div>
+            <div className="flex flex-col gap-[14px]">
+              <h3 className="text-body1_m md:text-h4_sb text-text1">
+                면접인원
+              </h3>
+              <p className=" text-subtext2 text-body2_m">그룹면접</p>
+            </div>
+          </div>
+          <div className="flex flex-col w-full max-w-[669px] gap-10">
+            <div>
+              <h3 className="text-body1_m md:text-h4_sb text-text1">
+                서류문항
+              </h3>
+              <div className="mt-6">
+                <p className="text-h4 text-text1 mb-[18px]">문항-1</p>
+                <div className="flex flex-col gap-[14px]">
+                  <input
+                    type="text"
+                    value={inputValue}
+                    placeholder="작성된 문항"
+                    className="w-full px-[22px] py-[14px] rounded-xl text-body1_r bg-searchbar text-black focus:outline-none"
+                  />
+                  <input
+                    type="text"
+                    value={inputValue}
+                    placeholder="어떤 문항을 작성할까용가리가리가리"
+                    className="w-full px-[22px] py-[14px] rounded-xl text-body1_r bg-searchbar text-black focus:outline-none"
+                  />
+                </div>
+              </div>
+              <div className="mt-6">
+                <p className="text-h4 text-text1 mb-[18px]">문항-1</p>
+                <div className="flex flex-col gap-[14px]">
+                  <input
+                    type="text"
+                    value={inputValue}
+                    placeholder="작성된 문항"
+                    className="w-full px-[22px] py-[14px] rounded-xl text-body1_r bg-searchbar text-black focus:outline-none"
+                  />
+                  <input
+                    type="text"
+                    value={inputValue}
+                    placeholder="어떤 문항을 작성할까용가리가리가리"
+                    className="w-full px-[22px] py-[14px] rounded-xl text-body1_r bg-searchbar text-black focus:outline-none"
+                  />
+                </div>
+              </div>
+            </div>
+            <div>
+              <h3 className="text-body1_m md:text-h4_sb text-text1">
+                면접문항
+              </h3>
+              <div className="mt-6">
+                <p className="text-h4 text-text1 mb-[18px]">문항-1</p>
+                <div className="flex flex-col gap-[14px]">
+                  <input
+                    type="text"
+                    value={inputValue}
+                    placeholder="작성된 문항"
+                    className="w-full px-[22px] py-[14px] rounded-xl text-body1_r bg-searchbar text-black focus:outline-none"
+                  />
+                  <input
+                    type="text"
+                    value={inputValue}
+                    placeholder="어떤 문항을 작성할까용가리가리가리"
+                    className="w-full px-[22px] py-[14px] rounded-xl text-body1_r bg-searchbar text-black focus:outline-none"
+                  />
+                </div>
+              </div>
+              <div className="mt-6">
+                <p className="text-h4 text-text1 mb-[18px]">문항-1</p>
+                <div className="flex flex-col gap-[14px]">
+                  <input
+                    type="text"
+                    value={inputValue}
+                    placeholder="작성된 문항"
+                    className="w-full px-[22px] py-[14px] rounded-xl text-body1_r bg-searchbar text-black focus:outline-none"
+                  />
+                  <input
+                    type="text"
+                    value={inputValue}
+                    placeholder="어떤 문항을 작성할까용가리가리가리"
+                    className="w-full px-[22px] py-[14px] rounded-xl text-body1_r bg-searchbar text-black focus:outline-none"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <IconBtn
+          type={"declaration"}
+          size={"large"}
+          title={"신고하기"}
+          onClick={toggleBottomModal}
+        />
+      </div>
+
+      {isBottomModalOpen && (
+        <ReportModal
+          onClose={() => setIsBottomModalOpen(false)}
+          onSubmit={handleReportSubmit}
+        />
+      )}
+      {alertMessage && (
+        <Alert text={alertMessage} onClose={() => setAlertMessage(null)} />
+      )}
     </div>
   );
 };
