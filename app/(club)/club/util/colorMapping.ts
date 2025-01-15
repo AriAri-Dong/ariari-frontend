def158a6b51ca75788314132da86e083c8bb23b4
bsu0404 <bsu0404@naver.com>
2025-01-15 22:57:58 +0900

    feat: Add util token color mapping

---
+++ app/(club)/club/util/colorMapping.ts
@@ -0,0 +1,30 @@
+export const colorMapping = {
+  C_TOKEN_1: {
+    bg: "bg-token_1_bg",
+    text: "text-token_1",
+  },
+  C_TOKEN_2: {
+    bg: "bg-token_2_bg",
+    text: "text-token_2",
+  },
+  C_TOKEN_3: {
+    bg: "bg-token_3_bg",
+    text: "text-token_3",
+  },
+  C_TOKEN_4: {
+    bg: "bg-token_4_bg",
+    text: "text-token_4",
+  },
+  C_TOKEN_5: {
+    bg: "bg-token_5_bg",
+    text: "text-token_5",
+  },
+  C_TOKEN_6: {
+    bg: "bg-token_6_bg",
+    text: "text-token_6",
+  },
+  C_TOKEN_7: {
+    bg: "bg-token_7_bg",
+    text: "text-token_7",
+  },
+};
