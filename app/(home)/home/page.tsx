2231adc16b9014a545c23bc74d1d8a1dc06473c3
inf0craw1 <inf0craw1@naver.com>
2025-01-17 19:48:26 +0900

    feat: Add save token to store

--- app/(home)/home/page.tsx
+++ app/(home)/home/page.tsx
@@ -1,8 +1,18 @@
+"use client";
+
 import ClubRanking from "@/pages/home/clubRanking";
 import LatestRecruitment from "./content/latestRecruitment";
 import PopularRecruitment from "./content/popularRecruitment";
+import { useUserStore } from "@/providers/user-store-provider";
+import { useEffect } from "react";
 
 const Home = () => {
+  const { accessToken, refreshToken } = useUserStore((state) => state);
+
+  useEffect(() => {
+    console.log("accessToken", accessToken);
+    console.log("refreshToken", refreshToken);
+  }, [accessToken, refreshToken]);
   return (
     <div className="w-full ">
       <ClubRanking />
