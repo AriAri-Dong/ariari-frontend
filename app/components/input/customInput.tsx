d0d9ccf03db9e1c87a792bff96cc72df69779006
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2025-01-22 17:31:21 +0900

    feat: Update customInput.tsx

--- app/components/input/customInput.tsx
+++ app/components/input/customInput.tsx
@@ -4,6 +4,7 @@ interface InputProps {
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   className?: string;
   disable?: boolean;
+  maxLength?: number;
 }
 
 const CustomInput = ({
@@ -12,12 +13,14 @@ const CustomInput = ({
   onChange,
   className = "",
   disable = false,
+  maxLength,
 }: InputProps) => (
   <input
     type="text"
     value={value}
     placeholder={placeholder}
     onChange={onChange}
+    maxLength={maxLength}
     className={`w-full p-3 rounded-xl text-mobile_body1_r bg-searchbar text-black
       md:text-body1_r md:px-[22px] md:py-[14px] focus:outline-none ${className}
       ${disable ? "cursor-pointer" : "cursor-default"}`}
