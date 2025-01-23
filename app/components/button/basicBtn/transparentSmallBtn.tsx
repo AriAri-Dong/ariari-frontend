affb7b1e2593752706acdca15351830032266f98
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2025-01-23 18:00:43 +0900

    style: Update transparentSmallBtn.tsx

--- app/components/button/basicBtn/transparentSmallBtn.tsx
+++ app/components/button/basicBtn/transparentSmallBtn.tsx
@@ -22,8 +22,8 @@ const TransparentSmallBtn = ({
 }: ButtonProps) => {
   return (
     <button
-      className={`py-2 px-[18px] md:py-3 md:px-[21px] text-13 md:text-15
-        font-medium text-primary border border-primary active:bg-selectedoption_hover
+      className={`flex text-nowrap items-center h-9 px-[18px] md:h-[44px] md:px-[22px]
+        text-mobile_body2_sb md:text-body1_sb text-primary border border-primary active:bg-selectedoption_hover
         bg-selectedoption_default md:hover:bg-selectedoption_hover md:active:bg-selectedoption_pressed
         ${round ? "rounded-full" : "rounded-lg"} ${className}`}
       onClick={(e) => {
