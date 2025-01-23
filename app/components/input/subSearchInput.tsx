c035c5e3ce64c2d77560ae91af411c78f4c38ef8
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2025-01-23 17:57:56 +0900

    feat: Create a subSearchInput component

---
+++ app/components/input/subSearchInput.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+import searchIcon from "@/images/icon/search.svg";
+
+interface SubSearchInputProps {
+  onSearch: (searchTerm: string) => void;
+  placeholder: string;
+  className?: string;
+}
+
+const SubSearchInput = ({
+  onSearch,
+  placeholder,
+  className,
+}: SubSearchInputProps) => {
+  const [inputValue, setInputValue] = useState<string>("");
+
+  return (
+    <div
+      className={`relative w-full bg-background focus-within:border-searchbarborder border 
+        border-menuborder rounded-3xl h-10 md:h-12 md:max-w-[270px] ${className}`}
+    >
+      <Image
+        src={searchIcon}
+        alt="search"
+        width={20}
+        height={20}
+        className="absolute left-3 top-1/2 transform -translate-y-1/2 md:left-4 md:w-6 md:h-6"
+      />
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        placeholder={placeholder || "검색어를 입력해주세요."}
+        className="w-full h-full pl-10 pr-4 text-text1 text-mobile_body2_r
+        md:text-body1_r focus:outline-none rounded-3xl"
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            onSearch;
+          }
+        }}
+      />
+    </div>
+  );
+};
+
+export default SubSearchInput;
