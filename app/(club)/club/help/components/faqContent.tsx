f0095ef11e3072fbd7e2dd1c93abccd796cf78b3
bsu0404 <bsu0404@naver.com>
2025-01-27 12:23:14 +0900

    fix: restore button removed during merge conflict

--- app/(club)/club/help/components/faqContent.tsx
+++ app/(club)/club/help/components/faqContent.tsx
@@ -166,11 +166,7 @@ const QnaFormContent = ({
           <LargeBtn onClick={handleSubmit} title={"등록하기"} />
         </div>
         <div className="hidden md:block">
-          <SmallBtn
-            onClick={handleSubmit}
-            title={"등록하기"}
-            className="hidden"
-          />
+          <SmallBtn onClick={handleSubmit} title={"등록하기"} />
         </div>
       </div>
       {alertMessage && (
