c66ec949b91e8f47ce0cb6fb632707c62d4f4bc6
inf0craw1 <inf0craw1@naver.com>
2025-01-28 18:28:49 +0900

    feat: Add FirstLoginModal

--- app/(home)/home/page.tsx
+++ app/(home)/home/page.tsx
@@ -4,20 +4,38 @@ import ClubRanking from "@/pages/home/clubRanking";
 import LatestRecruitment from "./content/latestRecruitment";
 import PopularRecruitment from "./content/popularRecruitment";
 import { useUserStore } from "@/providers/user-store-provider";
-import { useEffect } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import ProfileSettingModal from "@/components/modal/profileSetting/profileSettingModal";
+import useResponsive from "../../../hooks/useResponsive";
+import MobileProfileSettingModal from "@/components/modal/profileSetting/mobile/mobileProfileSettingModal";
+import { useState } from "react";
 
 const Home = () => {
-  const { accessToken, refreshToken } = useUserStore((state) => state);
+  const router = useRouter();
+  const isMdUp = useResponsive("md");
+  const searchParams = useSearchParams();
+  const firstLogin = searchParams.get("firstLogin");
+  const [isFirstLoginModalOpen, setIsFirstLoginModalOpen] = useState(
+    firstLogin === "1"
+  );
 
-  useEffect(() => {
-    console.log("accessToken", accessToken);
-    console.log("refreshToken", refreshToken);
-  }, [accessToken, refreshToken]);
+  const handleFirstLoginModalClose = () => {
+    setIsFirstLoginModalOpen(false);
+    router.replace("/", undefined);
+  };
+
   return (
     <div className="w-full ">
       <ClubRanking />
       <PopularRecruitment />
       <LatestRecruitment />
+      {firstLogin &&
+        isFirstLoginModalOpen &&
+        (isMdUp ? (
+          <ProfileSettingModal onClose={handleFirstLoginModalClose} />
+        ) : (
+          <MobileProfileSettingModal onClose={handleFirstLoginModalClose} />
+        ))}
     </div>
   );
 };
