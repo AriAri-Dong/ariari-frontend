124bfa9bb213bd8403ac0a4f33ebefd0d33aaec0
yeoniii20 <commetoi_yeoni@naver.com>
2025-01-31 00:00:17 +0900

    chore: next config setting

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  reactStrictMode: false,
+  swcMinify: false,
+  compiler: {
+    styledComponents: true,
+  },
+  output: "standalone",
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+    return config;
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/:path*`,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
