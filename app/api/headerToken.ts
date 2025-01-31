230ec9c681195d46528c2919d0808226799461b4
inf0craw1 <inf0craw1@naver.com>
2025-01-31 20:57:33 +0900

    refactor: Remove Bearer at axios header authorization

--- app/api/headerToken.ts
+++ app/api/headerToken.ts
@@ -3,7 +3,7 @@ import api from "@/api";
 export default class HeaderToken {
   public static set = (token: string | null): void => {
     if (token) {
-      api.defaults.headers.common.Authorization = `Bearer ${token}`;
+      api.defaults.headers.common.Authorization = `${token}`;
     } else {
       delete api.defaults.headers.common.Authorization;
     }
