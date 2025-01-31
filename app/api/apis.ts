c92c7e420e0904452ccd74ddcfa7edb63fbbf8cb
inf0craw1 <inf0craw1@naver.com>
2025-01-31 20:57:44 +0900

    feat: Add getUserData function

--- app/api/apis.ts
+++ app/api/apis.ts
@@ -1,10 +1,18 @@
 import api from "@/api";
-import { AuthResponseType } from "@/api/types";
+import { AuthResponseType, UserDataResponseType } from "@/api/types";
 
 export const getTokenWithCode = async (code: string) => {
   const url = `/login/kakao?code=${code}`;
 
   const { data } = await api.get<AuthResponseType>(url);
+
+  return data;
+};
+
+export const getUserData = async () => {
+  const url = `/member`;
+
+  const { data } = await api.get<UserDataResponseType>(url);
 
   return data;
 };
