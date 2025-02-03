cf32b5c3542eab750effa61953726ae2dcaabc88
bsu0404 <bsu0404@naver.com>
2025-02-04 00:43:53 +0900

    feat: Update MainRecruitmentCardProps with additional fields

--- app/types/components/card.ts
+++ app/types/components/card.ts
@@ -1,19 +1,24 @@
 import { StaticImageData } from "next/image";
+import { ClubFieldType, ClubRegionType, ParticipantType } from "../club";
+import { ProcedureType } from "@/types/recruitment";
 
 export interface MainRecruitmentCardProps {
   id: number;
-  date: string;
+  startDate: string;
+  endDate: string;
   clubName: string;
   clubImageUrl?: string | StaticImageData;
   title: string;
   imageUrl: string | StaticImageData;
+  limits: number;
   tag: {
     affiliation: string;
-    field: string;
-    region: string;
-    target: string;
+    field: ClubFieldType;
+    region: ClubRegionType;
+    target: ParticipantType;
   };
   isScrap: boolean;
+  procedureType: ProcedureType;
 }
 export interface GuideData {
   title: string;
