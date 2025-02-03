013ddd75c115ad3b7ec9e3f47664a368ce9b9b08
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2025-02-03 17:36:44 +0900

    style: Update clubNoticeHeader.tsx

--- app/(club)/club/management/activity/notice/components/clubNoticeHeader.tsx
+++ app/(club)/club/management/activity/notice/components/clubNoticeHeader.tsx
@@ -2,7 +2,7 @@ const ClubNoticeHeader = () => {
   return (
     <div
       className="flex-row justify-between items-center w-full my-2.5 hidden md:flex
-              py-2 pl-6 pr-[125px] rounded-lg bg-white70 text-subtext2 text-body1_m"
+              py-2 pl-5 pr-[125px] rounded-lg bg-white70 text-subtext2 text-body1_m"
     >
       <div className="flex gap-5">
         <p className="w-[76px] text-center">번호</p>
