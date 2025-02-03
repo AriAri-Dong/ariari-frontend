6808c98e9222abda270d395e05311ab7a38f74cd
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2025-02-03 17:42:07 +0900

    feat: Add header props

--- app/(club)/club/management/activity/notice/components/clubNoticeHeader.tsx
+++ app/(club)/club/management/activity/notice/components/clubNoticeHeader.tsx
@@ -1,8 +1,14 @@
-const ClubNoticeHeader = () => {
+interface ClubNoticeHeaderProps {
+  role?: string;
+}
+
+const ClubNoticeHeader = ({ role }: ClubNoticeHeaderProps) => {
   return (
     <div
-      className="flex-row justify-between items-center w-full my-2.5 hidden md:flex
-              py-2 pl-5 pr-[125px] rounded-lg bg-white70 text-subtext2 text-body1_m"
+      className={`flex-row justify-between items-center w-full my-2.5 hidden md:flex
+              py-2 pl-5 ${
+                role === "ADMIN" ? "pr-[96px]" : "pr-[125px]"
+              } rounded-lg bg-white70 text-subtext2 text-body1_m`}
     >
       <div className="flex gap-5">
         <p className="w-[76px] text-center">번호</p>
