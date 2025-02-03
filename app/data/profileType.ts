4376d13d21d042d5118d22f4a1959d3356f1b498
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2025-02-03 17:35:40 +0900

    feat: Add a profileType data

---
+++ app/data/profileType.ts
@@ -0,0 +1,16 @@
+import { profileType } from "@/models/member";
+
+export const PROFILE_TYPES: profileType[] = [
+  "ARIARI_MOUSE",
+  "ARIARI_COW",
+  "ARIARI_TIGER",
+  "ARIARI_RABBIT",
+  "ARIARI_DRAGON",
+  "ARIARI_SNAKE",
+  "ARIARI_HORSE",
+  "ARIARI_SHEEP",
+  "ARIARI_MONKEY",
+  "ARIARI_CHICKEN",
+  "ARIARI_DOG",
+  "ARIARI_PIG",
+];
