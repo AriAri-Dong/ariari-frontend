6e7b82c76ce2a7dd274e673839241625278ed5d6
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2025-02-04 12:22:39 +0900

    feat: Update page.tsx

--- app/(club)/club/management/activity/accounting/page.tsx
+++ app/(club)/club/management/activity/accounting/page.tsx
@@ -2,17 +2,60 @@
 
 import LeftMenu from "@/(club)/club/components/menu/leftMenu";
 import MobileMenu from "@/(club)/club/components/menu/mobileMenu";
+import Alert from "@/components/alert/alert";
 import MembershipBalanceBar from "@/components/bar/membershipBalanceBar";
 import MobileMembershipBalanceBar from "@/components/bar/mobileMembershipBalanceBar";
+import AccountingBottomSheet from "@/components/bottomSheet/accountingBottomSheet";
 import WriteBtn from "@/components/button/iconBtn/writeBtn";
 import PlusBtn from "@/components/button/withIconBtn/plusBtn";
 import MembershipBalanceList from "@/components/list/membershipBalanceList";
+import AccountingModal from "@/components/modal/club/accountingModal";
 import { TRANSACTIONS } from "@/data/clubAccounting";
 import useResponsive from "@/hooks/useResponsive";
+import { MembershipBalance } from "@/types/club";
+import { formatDate } from "@/utils/formatDateToDot";
+import { useState } from "react";
 
 const AccountingPage = () => {
   const isMdUp = useResponsive("md");
-  const handleWrite = () => {};
+
+  const [openWrite, setOpenWrite] = useState<boolean>(false);
+  const [transactions, setTransactions] =
+    useState<MembershipBalance[]>(TRANSACTIONS);
+  const [alertMessage, setAlertMessage] = useState<string | null>(null);
+
+  const handleClose = () => {
+    setOpenWrite(false);
+  };
+
+  const handleSubmit = (data: {
+    date: Date | null;
+    transaction: boolean | null;
+    amount: number;
+    details: string;
+  }) => {
+    if (!data.date) {
+      console.error("날짜가 없습니다.");
+      return;
+    }
+
+    const formattedDate = formatDate(data.date);
+
+    const newTransaction = {
+      date: formattedDate,
+      title: data.details,
+      amount: data.transaction ? data.amount : -data.amount,
+      balance:
+        transactions.length > 0
+          ? transactions[transactions.length - 1].balance +
+            (data.transaction ? data.amount : -data.amount)
+          : data.amount,
+    };
+
+    setTransactions((prev) => [...prev, newTransaction]);
+    handleClose();
+    setAlertMessage("회계내역이 등록되었습니다.");
+  };
 
   return (
     <div className="bg-sub_bg flex justify-center items-center w-full pb-20 md:pb-[124px]">
@@ -36,16 +79,35 @@ const AccountingPage = () => {
               />
             )}
             {/* 리스트 영역 */}
-            <MembershipBalanceList transactions={TRANSACTIONS} />
+            <MembershipBalanceList transactions={transactions} />
             <div className="flex justify-center mt-9 md:mt-10">
               <PlusBtn title={"더보기"} onClick={() => {}} />
             </div>
           </div>
         </div>
+        {/* 컴포넌트화 필요 */}
+        {isMdUp ? (
+          <div className="fixed w-full bottom-5 px-5 flex justify-end md:bottom-[44px] md:max-w-[1248px] md:px-5">
+            <WriteBtn onClick={() => setOpenWrite(true)} />
+          </div>
+        ) : (
+          <div className="fixed bottom-[77px] right-5 md:hidden">
+            <WriteBtn onClick={() => setOpenWrite(true)} />
+          </div>
+        )}
+        {openWrite &&
+          (isMdUp ? (
+            <AccountingModal onClose={handleClose} onSubmit={handleSubmit} />
+          ) : (
+            <AccountingBottomSheet
+              onClose={handleClose}
+              onSubmit={handleSubmit}
+            />
+          ))}
       </div>
-      <div className="fixed bottom-[77px] right-5 md:hidden">
-        <WriteBtn onClick={handleWrite} />
-      </div>
+      {alertMessage && (
+        <Alert text={alertMessage} onClose={() => setAlertMessage(null)} />
+      )}
     </div>
   );
 };
