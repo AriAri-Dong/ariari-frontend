0b0697b2345e364d5c86d2ee0cade80db6da552c
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2025-02-04 10:44:29 +0900

    feat: Create memvershipBar components

---
+++ app/components/bar/mobileMembershipBalanceBar.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { MembershipBalanceBarProps } from "./membershipBalanceBar";
+
+/**
+ * 포인트 현황 플로팅 바 컴포넌트
+ * @param currentPoint 현재 포인트
+ * @param className
+ * @returns
+ */
+
+const MobileMembershipBalanceBar = ({
+  currentPoint,
+  className,
+}: MembershipBalanceBarProps) => {
+  return (
+    <div className={`w-full flex justify-center ${className}`}>
+      <div
+        className="w-full bg-selectedoption_default rounded-lg
+        px-5 py-[18px]"
+      >
+        <div className="flex justify-between items-center">
+          <h3 className="text-body1_sb text-primary">동아리 회비 잔액</h3>
+          <div className="flex items-center gap-0.5 text-primary text-h4_sb">
+            <p>{currentPoint}</p>
+            <p>원</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MobileMembershipBalanceBar;
