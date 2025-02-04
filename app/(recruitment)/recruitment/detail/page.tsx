6827c685a39f725308826ff5be1f9ea218f8c8b0
bsu0404 <bsu0404@naver.com>
2025-02-04 14:33:38 +0900

    feat: Connect API to recruitment detail page

--- app/(recruitment)/recruitment/detail/page.tsx
+++ app/(recruitment)/recruitment/detail/page.tsx
@@ -1,72 +1,91 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
+
 import ClubInfo from "./content/clubInfo";
 import ClubActivities from "./content/clubActivities";
-import { RECRUITMENT_CARD } from "@/data/main";
+
+import { MainRecruitmentCardProps } from "@/types/components/card";
+import { RecruitmentData, RecruitmentNoteData } from "@/types/recruitment";
+import { transformRecruitmentToMainCard } from "./util/transformRecruitmentToMainCard";
+
+import { getClubRecruitmentList, getRecruitmentDetail } from "@/api/api";
+
+//테스트용 => /recruitment/detail?id=673127617679043209&clubId=673127617498687669
 
 const RecruitmentDetailPage = () => {
-  const recruitmentId = Math.floor(Math.random() * 17) + 1;
+  const params = useSearchParams();
+  const recruitmentId = params.get("id");
+  const clubId = params.get("clubId");
 
-  const recruitmentData = RECRUITMENT_CARD.find(
-    (item) => item.id === recruitmentId
-  );
+  // 이전 모집공고
+  const [prevRecruitmentList, setPrevRecruitmentList] = useState<
+    RecruitmentData[]
+  >([]);
+  // 변환된 모집 정보 정보
+  const [recruitmentData, setRecruitmentData] =
+    useState<MainRecruitmentCardProps | null>(null);
+  // 추가 질문 정보
+  const [recruitmentNoteDataList, setRecruitmentNoteDataList] = useState<
+    RecruitmentNoteData[] | null
+  >(null);
 
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (recruitmentId) {
+      const fetchRecruitmentDetail = async () => {
+        try {
+          setLoading(true);
+          setRecruitmentData(null);
+          const data = await getRecruitmentDetail(recruitmentId);
+          const parsedData = transformRecruitmentToMainCard(data);
+
+          setRecruitmentData(parsedData);
+          setRecruitmentNoteDataList(data.recruitmentNoteDataList);
+        } catch (error) {
+          console.error(error);
+        } finally {
+          setLoading(false);
+        }
+      };
+
+      fetchRecruitmentDetail();
+    }
+    if (clubId) {
+      const fetchAccectanceReview = async () => {
+        try {
+          const data = await getClubRecruitmentList(clubId);
+          setPrevRecruitmentList(data.recruitmentDataList);
+        } catch (error) {
+          console.error(error);
+        }
+      };
+      fetchAccectanceReview();
+    }
+  }, [recruitmentId, clubId]);
+
+  if (loading) {
+    return <h4 className="w-full text-center text-h4">로딩 중...</h4>;
+  }
   if (!recruitmentData) {
-    return <div>데이터를 찾을 수 없습니다.</div>;
+    return (
+      <h4 className="w-full text-center text-h4">
+        모집공고 데이터를 찾을 수 없습니다.
+      </h4>
+    );
   }
 
   return (
     <>
       <ClubInfo recruitmentData={recruitmentData} />
-      <ClubActivities recruitmentData={recruitmentData} />
+      <ClubActivities
+        recruitmentNoteDataList={recruitmentNoteDataList}
+        prevRecruitmentList={prevRecruitmentList || []}
+      />
     </>
   );
 };
 
 export default RecruitmentDetailPage;
-
-// "use client";
-
-// import React, { useState, useEffect } from "react";
-// import { useSearchParams } from "next/navigation";
-// import ClubActivities from "./content/clubActivities";
-// import ClubInfo from "./content/clubInfo";
-// import { RECRUITMENT_CARD } from "@/data/main";
-// import { MainRecruitmentCardProps } from "@/types/components/card";
-
-// const RecruitmentDetailPage = () => {
-//   const params = useSearchParams();
-//   const id = params.get("id");
-
-//   // recruitmentData를 단일 객체로 변경
-//   const [recruitmentData, setRecruitmentData] =
-//     useState<MainRecruitmentCardProps | null>(null);
-
-//   useEffect(() => {
-//     if (id) {
-//       const foundData = RECRUITMENT_CARD.find((item) => item.id === Number(id));
-//       if (foundData) {
-//         setRecruitmentData(foundData);
-//       }
-//     }
-//   }, [id]);
-
-//   return (
-//     <>
-//       {/* recruitmentData가 없으면 로딩 또는 에러 메시지 보여주기 */}
-//       {recruitmentData ? (
-//         <>
-//           <ClubInfo recruitmentData={recruitmentData} />
-//           <ClubActivities recruitmentData={recruitmentData} />
-//         </>
-//       ) : (
-//         <>
-//           <h1>로딩 페이지 필요</h1>
-//         </>
-//       )}
-//     </>
-//   );
-// };
-
-// export default RecruitmentDetailPage;
