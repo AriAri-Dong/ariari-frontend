e332b9bfb4dfc71afa771f5923cd2e0ac9f8b39d
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2025-02-05 16:58:37 +0900

    feat: Create axiosInstance

--- app/api/axiosInstance.ts
+++ app/api/axiosInstance.ts
@@ -13,31 +13,4 @@ const axiosInstance = axios.create({
   },
 });
 
-// // == 요청 인터셉터 추가 ==
-// axiosInstance.interceptors.request.use(
-//   (config) => {
-//     const token = localStorage.getItem("token");
-//     if (token) {
-//       config.headers["Authorization"] = `Bearer ${token}`;
-//     }
-//     return config;
-//   },
-//   (error) => {
-//     return Promise.reject(error);
-//   }
-// );
-
-// // == 응답 인터셉터 ==
-// axiosInstance.interceptors.response.use(
-//   (response) => {
-//     return response;
-//   },
-//   (error) => {
-//     if (error.response && error.response.status === 401) {
-//       window.location.href = "/login";
-//     }
-//     return Promise.reject(error);
-//   }
-// );
-
 export default axiosInstance;
