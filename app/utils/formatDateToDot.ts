f024c04f594a3b5af5eb4a1586e3dc67231a0c06
bsu0404 <bsu0404@naver.com>
2025-02-07 18:24:36 +0900

    feat: Add custom props to formatDateToDot

--- app/utils/formatDateToDot.ts
+++ app/utils/formatDateToDot.ts
@@ -1,9 +1,22 @@
-function formatDateToDot(dateString: string): string {
+// 기본값: 2025.01.01
+// withSpace === false : 2025. 02. 02
+// shortYear === true : 25.01.01
+
+function formatDateToDot(
+  dateString: string,
+  withSpace: boolean = true,
+  shortYear: boolean = false
+): string {
   const date = new Date(dateString);
-  const year = date.getFullYear();
+  const year = shortYear
+    ? String(date.getFullYear()).slice(2)
+    : String(date.getFullYear());
   const month = String(date.getMonth() + 1).padStart(2, "0");
   const day = String(date.getDate()).padStart(2, "0");
 
-  return `${year}. ${month}. ${day}`;
+  const separator = withSpace ? ". " : ".";
+
+  return `${year}${separator}${month}${separator}${day}`;
 }
+
 export default formatDateToDot;
