aa6aaeb645c1a1061fd352457ebb0f8634fda4fc
inf0craw1 <inf0craw1@naver.com>
2025-02-08 00:56:21 +0900

    feat: Add get Data after sign in

--- app/auth/kakao/page.tsx
+++ app/auth/kakao/page.tsx
@@ -2,28 +2,39 @@
 
 import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect } from "react";
-import { getTokenWithCode } from "@/api/apis.ts";
+import { getTokenWithCode, getUserData } from "@/api/apis.ts";
 import { useUserStore } from "@/providers/user-store-provider";
 import HeaderToken from "@/api/headerToken";
+import { IoConstructOutline } from "react-icons/io5";
 
 export default function SignIn() {
   const router = useRouter();
   const searchParams = useSearchParams();
-  const { signIn } = useUserStore((state) => state);
+  const { signIn, setUserData } = useUserStore((state) => state);
 
   useEffect(() => {
     const kakaoCode = searchParams.get("code") || "";
 
-    getTokenWithCode(kakaoCode).then((res) => {
-      signIn({
-        accessToken: res.accessToken,
-        refreshToken: res.refreshToken,
-        isFirstLogin: res.isFirstLogin,
+    getTokenWithCode(kakaoCode)
+      .then(async (res1) => {
+        signIn({
+          accessToken: res1.accessToken,
+          refreshToken: res1.refreshToken,
+          isFirstLogin: res1.isFirstLogin,
+          isSignIn: true,
+        });
+
+        HeaderToken.set(res1.accessToken);
+
+        await getUserData().then((res2) => {
+          setUserData(res2);
+        });
+
+        router.replace(`/${res1.isFirstLogin ? "?firstLogin=1" : ""}`);
+      })
+      .catch(() => {
+        router.replace("/");
       });
-      HeaderToken.set(res.accessToken);
-      router.replace(`/${res.isFirstLogin ? "?firstLogin=1" : ""}`);
-    });
-    router.replace("/");
   }, [router, searchParams, signIn]);
 
   return (
