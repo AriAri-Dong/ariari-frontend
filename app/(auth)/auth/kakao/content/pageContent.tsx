0e93b6f162b08c0e7ca4d26b6ddaefa742bbbdb2
yeoniii20 <100350818+yeoniii20@users.noreply.github.com>
2025-02-28 11:58:14 +0900

    feat: Update pageContent.tsx

--- app/(auth)/auth/kakao/content/pageContent.tsx
+++ app/(auth)/auth/kakao/content/pageContent.tsx
@@ -1,52 +1,41 @@
 "use client";
 
 import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useUserStore } from "@/providers/user-store-provider";
-import HeaderToken from "@/api/headerToken";
 import LoginLoading from "./loginLoading";
-import { getMemberData, getTokenWithCode } from "@/api/login/api";
-import api from "@/api";
+import { setAccessToken } from "@/api/axiosInstance";
+import { getTokenWithCode } from "@/api/login/api";
 
 export default function SignInPageContent() {
   const router = useRouter();
   const searchParams = useSearchParams();
-  const [kakaoCode, setKakaoCode] = useState<string>("");
-  const { signIn, setUserData } = useUserStore((state) => state);
+  const { signIn } = useUserStore((state) => state);
 
   useEffect(() => {
-    // "code" 쿼리 파라미터 추출
-    const curKakaoCode = searchParams.get("code") || "";
-    if (curKakaoCode === "") return;
-    // 카카오 로그인 코드 상태 업데이트
-    setKakaoCode(curKakaoCode);
+    const curKakaoCode = searchParams.get("code");
+    if (!curKakaoCode) return;
 
-    getTokenWithCode(kakaoCode)
-      .then(async (res1) => {
+    (async () => {
+      try {
+        // 카카오 로그인 후 AccessToken 발급
+        const res1 = await getTokenWithCode(curKakaoCode);
+        const { accessToken, isFirstLogin } = res1;
+
         // 로그인 상태 업데이트
-        signIn({
-          accessToken: res1.accessToken,
-          isFirstLogin: res1.isFirstLogin,
-          isSignIn: true,
-        });
+        signIn({ accessToken, isFirstLogin, isSignIn: true });
 
-        // API 요청을 위한 헤더에 액세스 토큰 설정
-        HeaderToken.set(res1.accessToken);
-        api.defaults.headers.common["Authorization"] = `${res1.accessToken}`;
+        // axiosInstance에 accessToken 저장
+        setAccessToken(accessToken);
 
-        // 유저 정보 가져와서 상태 업데이트
-        await getMemberData().then((res2) => {
-          setUserData(res2);
-          console.log(res2);
-        });
-
-        // 첫 로그인 여부에 따라 리다이렉트 처리
-        router.replace(`/${res1.isFirstLogin ? "?firstLogin=1" : ""}`);
-      })
-      .catch(() => {
+        // 첫 로그인 여부에 따라 리디렉트 처리
+        router.replace(isFirstLogin ? "/?firstLogin=1" : "/");
+      } catch (error: any) {
+        console.error("Login error:", error);
         router.replace("/");
-      });
-  }, [kakaoCode, router, searchParams, setUserData, signIn]);
+      }
+    })();
+  }, [router, searchParams, signIn]);
 
   return <LoginLoading />;
 }
