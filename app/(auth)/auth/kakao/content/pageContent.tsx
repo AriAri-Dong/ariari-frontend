75674142ba109e4464ffb0a7002c7bb904e42831
yeoniii20 <100350818+yeoniii20@users.noreply.github.com>
2025-02-28 13:20:28 +0900

    feat: Update pageContent.tsx

--- app/(auth)/auth/kakao/content/pageContent.tsx
+++ app/(auth)/auth/kakao/content/pageContent.tsx
@@ -1,64 +1,60 @@
 "use client";
 
 import { useRouter, useSearchParams } from "next/navigation";
-import ProfileSettingModal from "@/components/modal/profileSetting/profileSettingModal";
-import useResponsive from "@/hooks/useResponsive";
-import MobileProfileSettingModal from "@/components/modal/profileSetting/mobile/mobileProfileSettingModal";
 import { useEffect, useState } from "react";
-import ClubRanking from "@/(home)/home/content/clubRanking";
-import PopularRecruitment from "@/(home)/home/content/popularRecruitment";
-import LatestRecruitment from "@/(home)/home/content/latestRecruitment";
-import { useUserStore } from "@/providers/user-store-provider";
+import HeaderToken from "@/api/headerToken";
+import LoginLoading from "./loginLoading";
+import { getTokenWithCode } from "@/api/login/api";
 import { getMemberData } from "@/api/member/api";
+import axiosInstance from "@/api/axiosInstance";
+import { useUserStore } from "@/providers/user-store-provider";
 
-const HomePageContent = () => {
+export default function SignInPageContent() {
   const router = useRouter();
-  const isMdUp = useResponsive("md");
   const searchParams = useSearchParams();
-  const [isFirstLogin, setIsFirstLogin] = useState<string | null>(null);
-  const [isFirstLoginModalOpen, setIsFirstLoginModalOpen] =
-    useState<boolean>(false);
-
-  const { setUserData } = useUserStore((state) => state);
-
-  const handleFirstLoginModalClose = () => {
-    setIsFirstLoginModalOpen(false);
-    router.replace("/", undefined);
-  };
+  const [kakaoCode, setKakaoCode] = useState<string>("");
+  const { signIn, setUserData } = useUserStore((state) => state);
 
   useEffect(() => {
-    const searchParamFirstLogin = searchParams.get("firstLogin");
-    setIsFirstLogin(searchParamFirstLogin);
-    setIsFirstLoginModalOpen(searchParamFirstLogin === "1");
+    // "code" 쿼리 파라미터 추출
+    const curKakaoCode = searchParams.get("code") || "";
+    if (curKakaoCode === "") return;
+    // 카카오 로그인 코드 상태 업데이트
+    setKakaoCode(curKakaoCode);
 
-    // 유저 정보 가져와서 상태 업데이트
-    (async () => {
-      try {
-        const res = await getMemberData();
-        if (res) {
-          setUserData(res);
-          console.log("유저 정보 불러오기 성공:", res);
+    getTokenWithCode(kakaoCode)
+      .then(async (res1) => {
+        // 로그인 상태 업데이트
+        signIn({
+          accessToken: res1.accessToken,
+          isFirstLogin: res1.isFirstLogin,
+          isSignIn: true,
+        });
+
+        // 토큰을 sessionStorage에 저장
+        if (typeof window !== "undefined") {
+          sessionStorage.setItem("accessToken", res1.accessToken);
         }
-      } catch (error) {
-        console.error("유저 정보 불러오기 실패:", error);
-      }
-    })();
-  }, [searchParams, setUserData]);
 
-  return (
-    <div className="w-full ">
-      <ClubRanking />
-      <PopularRecruitment />
-      <LatestRecruitment />
-      {isFirstLogin &&
-        isFirstLoginModalOpen &&
-        (isMdUp ? (
-          <ProfileSettingModal onClose={handleFirstLoginModalClose} />
-        ) : (
-          <MobileProfileSettingModal onClose={handleFirstLoginModalClose} />
-        ))}
-    </div>
-  );
-};
+        // API 요청을 위한 헤더에 액세스 토큰 설정
+        HeaderToken.set(res1.accessToken);
+        axiosInstance.defaults.headers.common[
+          "Authorization"
+        ] = `Bearer ${res1.accessToken}`;
 
-export default HomePageContent;
+        // 유저 정보 가져와서 상태 업데이트
+        await getMemberData().then((res2) => {
+          setUserData(res2);
+          console.log(res2);
+        });
+
+        // 첫 로그인 여부에 따라 리다이렉트 처리
+        router.replace(`/${res1.isFirstLogin ? "?firstLogin=1" : ""}`);
+      })
+      .catch(() => {
+        router.replace("/");
+      });
+  }, [kakaoCode, router, searchParams, setUserData, signIn]);
+
+  return <LoginLoading />;
+}
