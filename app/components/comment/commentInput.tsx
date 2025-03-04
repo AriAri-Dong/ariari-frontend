d4ffba0df91dc57e43210229c443b4fe22b8bd3b
bsu0404 <bsu0404@naver.com>
2025-03-04 23:41:17 +0900

    feat: Add placeholder and initialText props to CommentInput

--- app/components/comment/commentInput.tsx
+++ app/components/comment/commentInput.tsx
@@ -4,11 +4,30 @@ import useResponsive from "@/hooks/useResponsive";
 
 interface CommentInputProps {
   onSend: () => void;
+  placeholder?: string;
+  initialText?: string;
 }
 
-const CommentInput = ({ onSend }: CommentInputProps) => {
-  const [comment, setComment] = useState("");
+const CommentInput = ({
+  initialText = "",
+  placeholder,
+  onSend,
+}: CommentInputProps) => {
+  const [comment, setComment] = useState(initialText);
   const isMdUp = useResponsive("md");
+
+  const handleSubmit = () => {
+    if (comment.trim().length === 0) return;
+    onSend();
+    setComment("");
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      handleSubmit();
+    }
+  };
 
   return (
     <div className="w-full flex justify-between items-center p-1 pl-4 md:p-1.5 md:pl-[22px] bg-hover rounded-24 md:rounded-28 gap-3 md:gap-5">
@@ -16,14 +35,15 @@ const CommentInput = ({ onSend }: CommentInputProps) => {
         type="text"
         value={comment}
         onChange={(e) => setComment(e.target.value)}
-        placeholder="답변을 입력해주세요."
+        onKeyDown={handleKeyDown}
+        placeholder={placeholder ? placeholder : "댓글을 입력해주세요."}
         className="w-full py-1.5 bg-searchbar text-subtext1 placeholder:text-subtext2 text-mobile_body1_r md:py-1.5 md:text-body1_r focus:outline-none placeholder:text-subtext1"
       />
       <div className="flex items-center gap-4">
         {isMdUp && (
           <p className="text-body3_m text-unselected">{comment.length}/1000</p>
         )}
-        <SendBtn onClick={onSend} />
+        <SendBtn onClick={handleSubmit} />
       </div>
     </div>
   );
