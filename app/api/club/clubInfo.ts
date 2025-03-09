7191da9236fe511d15110e2152fe1a5c1f01ac2f
seyoun <lsy20140@gmail.com>
2025-03-09 15:34:03 +0900

    chore: Apply axiosInstance

--- app/api/club/clubInfo.ts
+++ app/api/club/clubInfo.ts
@@ -1,8 +1,9 @@
 import { ClubDetailRes } from "@/types/club";
-import api from "..";
+import axiosInstance from "../axiosInstance";
+import { CLUBS } from "../apiUrl";
 
 // 동아리 상세 정보 조회
 export const fetchClubInfo = async (id: string) => {
-  const res = await api.get<ClubDetailRes>(`/clubs/${id}`);
+  const res = await axiosInstance.get<ClubDetailRes>(`${CLUBS}/${id}`);
   return res.data;
 };
