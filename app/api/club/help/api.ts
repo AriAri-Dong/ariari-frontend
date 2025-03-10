1db3995ebd74d2e36b708534fa8789f2aff27bfc
seyoun <lsy20140@gmail.com>
2025-03-11 01:21:31 +0900

    feat: Add Q&A api and custom hook using useInfiniteQuery

--- app/api/club/help/api.ts
+++ app/api/club/help/api.ts
@@ -1,6 +1,6 @@
 import axiosInstance from "@/api/axiosInstance";
 import { Pageable } from "@/types/api";
-import { ClubFaqListRes } from "@/types/club";
+import { ClubFaqListRes, ClubQnaListRes } from "@/types/club";
 
 // 동아리 상세 FAQ 리스트 조회
 export const getClubFaqList = async (
@@ -22,6 +22,31 @@ export const getClubFaqList = async (
     console.log("faq list 조회 실패");
     return {
       clubFaqDataList: [],
+      pageInfo: { contentSize: 0, totalSize: 0, totalPages: 0 },
+    };
+  }
+};
+
+// 동아리 상세 Q&A 리스트 조회
+export const getClubQnaList = async (
+  clubId: string,
+  page: Pageable["page"]
+) => {
+  try {
+    const res = await axiosInstance.get<ClubQnaListRes>(
+      `/clubs/${clubId}/club-questions`,
+      {
+        params: {
+          page,
+          size: 10,
+        },
+      }
+    );
+    return res.data;
+  } catch (error) {
+    console.log("qna list 조회 실패");
+    return {
+      clubQuestionDataList: [],
       pageInfo: { contentSize: 0, totalSize: 0, totalPages: 0 },
     };
   }
--- app/hooks/club/useClubHelpQuery.tsx
+++ app/hooks/club/useClubHelpQuery.tsx
@@ -1,5 +1,5 @@
-import { getClubFaqList } from "@/api/club/help/api";
-import { ClubFaqListRes } from "@/types/club";
+import { getClubFaqList, getClubQnaList } from "@/api/club/help/api";
+import { ClubFaqListRes, ClubQnaListRes } from "@/types/club";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { AxiosError } from "axios";
 
@@ -22,7 +22,34 @@ export const useClubFaqQuery = (clubId: string) => {
 
   return {
     faqList: data?.pages.flatMap((page) => page.clubFaqDataList) ?? [],
-    totalSize: data?.pages[0].pageInfo.totalSize,
+    totalSize: data?.pages[0].pageInfo.totalSize ?? 0,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    isError,
+  };
+};
+
+// 동아리 상세 Q&A 리스트 조회
+export const useClubQnaQuery = (clubId: string) => {
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, isError } =
+    useInfiniteQuery<ClubQnaListRes, AxiosError>({
+      queryKey: ["club-questions", clubId],
+      queryFn: ({ pageParam = 0 }) =>
+        getClubQnaList(clubId, pageParam as number),
+      initialPageParam: 0,
+      getNextPageParam: (lastPage, allPages) => {
+        const nextPage = allPages.length;
+        const totalPages = lastPage.pageInfo.totalPages;
+
+        // 마지막 페이지 판단 기준 - 전체 페이지 수, 현재 불러진 페이지 수
+        return totalPages > nextPage ? nextPage : undefined;
+      },
+    });
+
+  return {
+    qnaList: data?.pages.flatMap((page) => page.clubQuestionDataList) ?? [],
+    totalSize: data?.pages[0].pageInfo.totalSize ?? 0,
     fetchNextPage,
     hasNextPage,
     isFetchingNextPage,
