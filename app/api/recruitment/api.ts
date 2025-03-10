32740515da898101d1fe3cd13fc1fb62390f3b89
bsu0404 <bsu0404@naver.com>
2025-03-10 17:47:29 +0900

    refactor: Relocate content

--- app/api/recruitment/api.ts
+++ app/api/recruitment/api.ts
@@ -1,3 +1,4 @@
+import { ClubRecruitmentListRes } from "@/types/recruitment";
 import { RECRUITMENT_RANKING } from "../apiUrl";
 import axiosInstance from "../axiosInstance";
 
@@ -12,5 +13,39 @@ export const getRecruitmentRanking = async () => {
     //   memberData: { id: "", nickname: "", profileType: "" },
     //   schoolData: { name: "" },
     // };
+  }
+};
+
+// 해당 동아리 모집공고 리스트
+export const getClubRecruitment = async (clubId: string) => {
+  try {
+    const { data } = await axiosInstance.get<ClubRecruitmentListRes>(
+      `/clubs/${clubId}/recruitments`
+    );
+    return data;
+  } catch (err) {
+    console.log("동아리 모집공고 리스트 조회 실패", err);
+  }
+};
+
+// 모집 종료
+export const endRecruitment = async (recruitmentId: string) => {
+  try {
+    const { data } = await axiosInstance.put(`/recruitments/${recruitmentId}`);
+    return data;
+  } catch (err) {
+    console.log("모집종료 실패", err);
+  }
+};
+
+// 모집 삭제
+export const deleteRecruitment = async (recruitmentId: string) => {
+  try {
+    const { data } = await axiosInstance.delete(
+      `/recruitments/${recruitmentId}`
+    );
+    return data;
+  } catch (err) {
+    console.log("모집삭제 실패", err);
   }
 };
