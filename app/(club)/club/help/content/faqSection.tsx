8a482f2028f45375f541b27580624d7bd19a9e5f
seyoun <lsy20140@gmail.com>
2025-03-11 01:18:19 +0900

    refactor: Split into faq and qna sections

---
+++ app/(club)/club/help/content/faqSection.tsx
@@ -0,0 +1,55 @@
+import { useClubFaqQuery } from "@/hooks/club/useClubHelpQuery";
+import { useSearchParams } from "next/navigation";
+import QuestionDropdown from "../components/questionDropdown";
+import { useClubContext } from "@/context/ClubContext";
+import { useEffect, useState } from "react";
+import PlusBtn from "@/components/button/withIconBtn/plusBtn";
+
+const FaqSection = ({
+  onListCount,
+}: {
+  onListCount: (cnt: number) => void;
+}) => {
+  const [selectedFaq, setSelectedFaq] = useState<number | null>(null);
+
+  const params = useSearchParams();
+  const clubId = params.get("clubId") ?? "";
+
+  const { clubInfo, role } = useClubContext();
+
+  const {
+    faqList,
+    totalSize,
+    hasNextPage,
+    fetchNextPage,
+    isFetchingNextPage,
+    isError,
+  } = useClubFaqQuery(clubId);
+
+  useEffect(() => {
+    onListCount(totalSize);
+  }, [onListCount]);
+
+  return (
+    <>
+      {faqList?.map((item, index) => (
+        <div key={index} className="mb-2.5">
+          <QuestionDropdown
+            data={item}
+            myRoleType={role}
+            myProfileType={clubInfo?.clubMemberData.profileType}
+            isOpen={item.id == selectedFaq}
+            setSelected={setSelectedFaq}
+          />
+        </div>
+      ))}
+      {hasNextPage && (
+        <div className="flex justify-center mt-9 md:mt-10">
+          <PlusBtn title={"더보기"} onClick={() => fetchNextPage()} />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default FaqSection;
---
+++ app/(club)/club/help/content/qnaSection.tsx
@@ -0,0 +1,54 @@
+import { useClubQnaQuery } from "@/hooks/club/useClubHelpQuery";
+import { useSearchParams } from "next/navigation";
+import QuestionDropdown from "../components/questionDropdown";
+import { useClubContext } from "@/context/ClubContext";
+import { useEffect, useState } from "react";
+import PlusBtn from "@/components/button/withIconBtn/plusBtn";
+
+const QnaSection = ({
+  onListCount,
+}: {
+  onListCount: (cnt: number) => void;
+}) => {
+  const [selectedQna, setSelectedQna] = useState<number | null>(null);
+
+  const params = useSearchParams();
+  const clubId = params.get("clubId") ?? "";
+  const { clubInfo, role } = useClubContext();
+
+  const {
+    qnaList,
+    totalSize,
+    hasNextPage,
+    fetchNextPage,
+    isFetchingNextPage,
+    isError,
+  } = useClubQnaQuery(clubId);
+
+  useEffect(() => {
+    onListCount(totalSize);
+  }, [onListCount]);
+
+  return (
+    <>
+      {qnaList.map((item, index) => (
+        <div key={index} className="mb-2.5">
+          <QuestionDropdown
+            data={item}
+            myRoleType={role}
+            myProfileType={clubInfo?.clubMemberData.profileType}
+            isOpen={item.id == selectedQna}
+            setSelected={setSelectedQna}
+          />
+        </div>
+      ))}
+      {hasNextPage && (
+        <div className="flex justify-center mt-9 md:mt-10">
+          <PlusBtn title={"더보기"} onClick={() => fetchNextPage()} />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default QnaSection;
