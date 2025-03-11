2a803305847f32e898a111d40da3a82ba34507ad
seyoun <lsy20140@gmail.com>
2025-03-12 02:13:13 +0900

    fix: Modify profileType to optional

--- app/types/member.ts
+++ app/types/member.ts
@@ -16,7 +16,7 @@ export type profileType =
 export interface MemberData {
   id: number; // $int64
   nickname: string;
-  profileType: profileType;
+  profileType?: profileType;
 }
 
 export interface ClubMemberData {
