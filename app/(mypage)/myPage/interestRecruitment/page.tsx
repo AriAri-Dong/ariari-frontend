480f64e5513c9c424e2a49b1231bd587f32d42b6
yeoniii20 <100350818+yeoniii20@users.noreply.github.com>
2025-03-12 16:15:11 +0900

    feat: Apply interestRecruitment api

--- app/(mypage)/myPage/interestRecruitment/page.tsx
+++ app/(mypage)/myPage/interestRecruitment/page.tsx
@@ -1,14 +1,20 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { POPULARITY_SORT_TYPE } from "@/data/pulldown";
 import HeaderSection from "./content/headerSection";
 import FilterSection from "./content/filterSection";
 import PlusBtn from "@/components/button/withIconBtn/plusBtn";
 import MainRecruitmentCard from "@/components/card/mainRecruitmentCard";
 import { RECRUITMENT_CARD } from "@/data/main";
+import { RecruitmentData, RecruitmentResponse } from "@/types/recruitment";
+import { getAllRecruitments } from "@/api/recruitment/api";
+import { ClubSearchCondition, Pageable } from "@/types/api";
 
 const InterestRecruitmentPage = () => {
+  const [recruitmentData, setRecruitmentData] = useState<RecruitmentData[]>([]);
+  const [page, setPage] = useState<number>(0);
+  const [more, setMore] = useState<boolean>(true);
   const [isDeadlineChecked, setIsDeadlineChecked] = useState<boolean>(false);
   const [isRecruitmentChecked, setIsRecruitmentChecked] =
     useState<boolean>(false);
@@ -24,6 +30,48 @@ const InterestRecruitmentPage = () => {
     setIsRecruitmentChecked(!isRecruitmentChecked);
   };
 
+  // 모집 목록
+  const fetchRecruitments = async (newPage = 0, reset = false) => {
+    try {
+      const condition: ClubSearchCondition = {
+        clubCategoryTypes: undefined,
+        clubRegionTypes: undefined,
+        participantTypes: undefined,
+      };
+
+      const pageable: Pageable = {
+        page: newPage,
+        size: 10,
+        sort: [""],
+      };
+
+      const response: RecruitmentResponse = await getAllRecruitments(
+        condition,
+        pageable
+      );
+
+      if (reset) {
+        setRecruitmentData(response.recruitmentDataList);
+        console.log("모집 리스트 조회 >>>", response);
+      } else {
+        setRecruitmentData((prev) => [
+          ...prev,
+          ...response.recruitmentDataList,
+        ]);
+      }
+
+      if (newPage + 1 >= response.pageInfo.totalPages) {
+        setMore(false);
+      }
+    } catch (error) {
+      console.error("Error fetching recruitments:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchRecruitments(0, true);
+  }, []);
+
   return (
     <div>
       <div className="flex flex-col gap-5 mt-[46px] md:mt-8 md:gap-[22px]">
@@ -35,17 +83,27 @@ const InterestRecruitmentPage = () => {
           toggleRecruitmentCheck={toggleRecruitmentCheck}
           sortType={sortType}
           setSortType={setSortType}
+          dataCount={recruitmentData.length || 0}
         />
       </div>
       <div className="flex flex-col mt-5 md:mt-7">
         <div
-          className="flex flex-col gap-5 md:gap-x-4 md:gap-y-7 md:flex-row md:flex-wrap
+          className="md:min-h-[450px] min-h-[300px] flex flex-col gap-5 md:gap-x-4 md:gap-y-7 md:flex-row md:flex-wrap
     md:grid md:grid-cols-3 lx:grid-cols-4"
         >
-          <MainRecruitmentCard data={RECRUITMENT_CARD} />
+          <MainRecruitmentCard data={recruitmentData} />
         </div>
         <div className="self-center mt-9 mb-[80px] md:mt-10 md:mb-[124px]">
-          <PlusBtn title={"더보기"} onClick={() => {}} />
+          {more && (
+            <PlusBtn
+              title={"더보기"}
+              onClick={() => {
+                const nextPage = page + 1;
+                setPage(nextPage);
+                fetchRecruitments(nextPage);
+              }}
+            />
+          )}
         </div>
       </div>
     </div>
