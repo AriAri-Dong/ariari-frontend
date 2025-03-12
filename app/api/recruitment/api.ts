6e020391f669620a9b6f831ddb72b31362788731
yeoniii20 <100350818+yeoniii20@users.noreply.github.com>
2025-03-12 16:11:49 +0900

    feat: Add get all recruitment list api

--- app/api/recruitment/api.ts
+++ app/api/recruitment/api.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
-import { RECRUITMENT_RANKING } from "../apiUrl";
+import { RECRUITMENT, RECRUITMENT_RANKING } from "../apiUrl";
 import axiosInstance from "../axiosInstance";
+import { ClubSearchCondition, Pageable } from "@/types/api";
+import { RecruitmentResponse } from "@/types/recruitment";
 
 export const getRecruitmentRanking = async () => {
   try {
@@ -13,5 +15,40 @@ export const getRecruitmentRanking = async () => {
     //   memberData: { id: "", nickname: "", profileType: "" },
     //   schoolData: { name: "" },
     // };
+  }
+};
+
+// 전체 모집리스트 조회
+export const getAllRecruitments = async (
+  condition: ClubSearchCondition,
+  pageable: Pageable
+): Promise<RecruitmentResponse> => {
+  try {
+    const filteredCondition = {
+      clubCategoryTypes: condition.clubCategoryTypes?.length
+        ? condition.clubCategoryTypes
+        : undefined,
+      clubRegionTypes: condition.clubRegionTypes?.length
+        ? condition.clubRegionTypes
+        : undefined,
+      participantTypes: condition.participantTypes?.length
+        ? condition.participantTypes
+        : undefined,
+    };
+
+    const params = {
+      ...filteredCondition,
+      page: pageable.page,
+      size: pageable.size,
+      ...(pageable.sort ? { sort: pageable.sort.join(",") } : {}),
+    };
+
+    const response = await axiosInstance.get<RecruitmentResponse>(RECRUITMENT, {
+      params,
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching recruitment info:", error);
+    throw error;
   }
 };
