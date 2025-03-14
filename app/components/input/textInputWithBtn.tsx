a8ec3b233db41b422ddf5e110837c73b1911a620
yeoniii20 <100350818+yeoniii20@users.noreply.github.com>
2025-03-14 11:58:23 +0900

    design: Add maxlength

--- app/components/input/textInputWithBtn.tsx
+++ app/components/input/textInputWithBtn.tsx
@@ -6,6 +6,7 @@ interface TextInputWithBtnProps {
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
   placeholder?: string;
   className?: string;
+  maxLength?: number;
   onClick: () => void;
   btnTitle: string;
 }
@@ -16,17 +17,25 @@ const TextInputWithBtn: React.FC<TextInputWithBtnProps> = ({
   placeholder = "Default Text",
   className = "",
   onClick,
+  maxLength,
   btnTitle,
 }) => {
   return (
     <div className={`flex items-center gap-[18px] w-full ${className}`}>
-      <input
-        className="w-full bg-searchbar text-mobile_body1_r text-text1 py-3 px-4 rounded-xl 
+      <div className={`w-full relative`}>
+        <input
+          className="w-full bg-searchbar text-mobile_body1_r text-text1 py-3 px-4 rounded-xl 
         focus:border focus:border-blue-500 focus:outline-none placeholder-subtext2 md:px-[22px] md:py-[13px] md:text-body1_r"
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
-      />
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+        />
+        {maxLength && (
+          <div className="absolute right-4 top-1/2 -translate-y-1/2 text-unselected text-mobile_body1_r md:right-[22px]">
+            {value.length}/{maxLength}
+          </div>
+        )}
+      </div>
       <TransparentSmallBtn title={btnTitle} onClick={onClick} round />
     </div>
   );
