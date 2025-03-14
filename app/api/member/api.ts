1239bf35c082d32acabe7557f1594abe5410c797
yeoniii20 <100350818+yeoniii20@users.noreply.github.com>
2025-03-14 09:18:12 +0900

    feat: Add member api

--- app/api/member/api.ts
+++ app/api/member/api.ts
@@ -1,7 +1,39 @@
 import { UserDataResponseType } from "@/types/api";
-import { MEMBERS, MEMBERS_MY } from "../apiUrl";
+import {
+  MEMBERS_MY_NICKNAME,
+  MEMBERS_MY_PROFILE,
+  MEMBERS,
+  MEMBERS_MY,
+} from "../apiUrl";
 import axiosInstance from "../axiosInstance";
 
+// 닉네임 변경 API
+export const updateNickname = async (nickname: string) => {
+  try {
+    const { data } = await axiosInstance.put(MEMBERS_MY_NICKNAME, { nickname });
+    console.log("닉네임 변경 성공 >>>", data);
+    return data;
+  } catch (err) {
+    console.error("닉네임 변경 실패:", err);
+    throw err;
+  }
+};
+
+// 프로필 변경 API
+export const updateProfileType = async (profileType: string) => {
+  try {
+    const { data } = await axiosInstance.put(MEMBERS_MY_PROFILE, {
+      profileType,
+    });
+    console.log("프로필 변경 성공 >>>", data);
+    return data;
+  } catch (err) {
+    console.error("프로필 변경 실패:", err);
+    throw err;
+  }
+};
+
+// 닉네임으로 회원 데이터 조회
 export const getMemberDataByNickname = async (nickname: string) => {
   try {
     const { data } = await axiosInstance.get<UserDataResponseType[]>(MEMBERS, {
@@ -15,6 +47,7 @@ export const getMemberDataByNickname = async (nickname: string) => {
   }
 };
 
+// 현재 로그인된 사용자 정보 조회
 export const getMemberData = async () => {
   try {
     const { data } = await axiosInstance.get<UserDataResponseType>(MEMBERS_MY);
