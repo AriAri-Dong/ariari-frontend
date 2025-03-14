6bd0b40502a799e6298c7a96ee7a5d2f4aafdb66
bsu0404 <bsu0404@naver.com>
2025-03-14 14:10:15 +0900

    feat: Create Loading component

---
+++ app/components/feedback/loading.tsx
@@ -0,0 +1,23 @@
+import Lottie from "lottie-react";
+import loading from "@/images/lottie/loading.json";
+
+interface LoadingProps {
+  className?: string;
+  lottieClassName?: string;
+}
+const Loading = ({ className, lottieClassName }: LoadingProps) => {
+  return (
+    <div
+      className={`flex bg-white flex-col items-center justify-center min-h-[300px] md:min-h-[450px] text-center ${className}`}
+    >
+      <Lottie
+        animationData={loading}
+        loop
+        autoPlay
+        className={`text-center w-40 h-40 ${lottieClassName}`}
+      />
+    </div>
+  );
+};
+
+export default Loading;
