f056a9245414604339208bedf8c47dca47573c51
seyoun <lsy20140@gmail.com>
2025-03-21 00:38:57 +0900

    feat: Add getApplicationsList api and custom query hook

--- app/api/apply/api.ts
+++ app/api/apply/api.ts
@@ -1,5 +1,7 @@
+import { Pageable } from "@/types/api";
 import { APPLY_TEMPS_MY } from "../apiUrl";
 import axiosInstance from "../axiosInstance";
+import { ApplicationListConditionReq, ApplyListRes } from "@/types/application";
 
 export const getAppliedList = async (
   page: number = 0,
@@ -25,6 +27,33 @@ export const getAppliedList = async (
     console.error("내 임시 지원 리스트 조회 실패:", err);
     return {
       data: [],
+    };
+  }
+};
+
+// 동아리 상세 - 지원 현황 리스트
+export const getApplicationsList = async (
+  clubId: string,
+  condition: ApplicationListConditionReq,
+  page: Pageable["page"]
+) => {
+  try {
+    const res = await axiosInstance.get<ApplyListRes>(
+      `club/${clubId}/applies`,
+      {
+        params: {
+          ...condition,
+          page: page,
+          size: 1,
+        },
+      }
+    );
+    return res.data;
+  } catch (error) {
+    console.log("Error fetching applications list", error);
+    return {
+      applyDataList: [],
+      pageInfo: { contentSize: 0, totalSize: 0, totalPages: 0 },
     };
   }
 };
---
+++ app/hooks/apply/useApplicationQuery.tsx
@@ -0,0 +1,55 @@
+import { getApplicationsList } from "@/api/apply/api";
+import { ApplicationListConditionReq, ApplyListRes } from "@/types/application";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
+import { AxiosError } from "axios";
+
+// 동아리 상세 지원 현황 리스트 조회
+export const useApplicationQuery = (
+  clubId: string,
+  condition: ApplicationListConditionReq
+) => {
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, isError } =
+    useInfiniteQuery<ApplyListRes, AxiosError>({
+      queryKey: ["club", "applications", clubId, JSON.stringify(condition)],
+      queryFn: ({ pageParam = 0 }) =>
+        getApplicationsList(clubId, condition, pageParam as number),
+      initialPageParam: 0,
+      getNextPageParam: (lastPage, allPages) => {
+        const nextPage = allPages.length;
+        const totalPages = lastPage.pageInfo.totalPages;
+
+        // 마지막 페이지 판단 기준 - 전체 페이지 수, 현재 불러진 페이지 수
+        return totalPages > nextPage ? nextPage : undefined;
+      },
+    });
+
+  // 전체/대기중 탭에 표기되는 항목 개수
+  const { data: fixedTotalSize } = useQuery<
+    { totalAllSize: number; totalPendingSize: number },
+    AxiosError
+  >({
+    queryKey: ["club", "applications", clubId, "fixed-total-size"],
+    queryFn: async () => {
+      const [allApplyRes, pendingApplyRes] = await Promise.all([
+        getApplicationsList(clubId, { isPendent: false }, 0),
+        getApplicationsList(clubId, { isPendent: true }, 0),
+      ]);
+      return {
+        totalAllSize: allApplyRes.pageInfo.totalSize,
+        totalPendingSize: pendingApplyRes.pageInfo.totalSize,
+      };
+    },
+    staleTime: Infinity,
+  });
+
+  return {
+    allTabSize: fixedTotalSize?.totalAllSize ?? 0,
+    pendingTabSize: fixedTotalSize?.totalPendingSize ?? 0,
+    applicationsList: data?.pages.flatMap((page) => page.applyDataList) ?? [],
+    totalSize: data?.pages[0].pageInfo.totalSize ?? 0,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    isError,
+  };
+};
