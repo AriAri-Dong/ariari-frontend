5243015245117c10f7b1b289d7c801aff0059ecc
seyoun <lsy20140@gmail.com>
2025-03-22 19:07:37 +0900

    refactor: Change pagination size 1 to 10

--- app/api/apply/api.ts
+++ app/api/apply/api.ts
@@ -235,7 +235,7 @@ export const getApplicationsList = async (
         params: {
           ...condition,
           page: page,
-          size: 1,
+          size: 10,
         },
       }
     );
