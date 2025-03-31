6edf6a2c702c47b2f95255272836cd96c273cb92
seyoun <lsy20140@gmail.com>
2025-03-31 17:32:41 +0900

    feat: Add club menu icons mapping object

--- app/constants/clubMenu.ts
+++ app/constants/clubMenu.ts
@@ -6,6 +6,22 @@ import {
   CLUB_LEFT_MENU_USER,
 } from "@/data/club";
 
+// menu icons
+import AcceptanceReviewIcon from "../images/icon/clubMenu/acceptance_review.svg?svgr";
+import AccountingIcon from "../images/icon/clubMenu/accounting.svg?svgr";
+import ActivityReviewIcon from "../images/icon/clubMenu/activity_review.svg?svgr";
+import ActivityHistoryIcon from "../images/icon/clubMenu/activity_history.svg?svgr";
+import ActivityManagementIcon from "../images/icon/clubMenu/activity_management.svg?svgr";
+import AnnouncementIcon from "../images/icon/clubMenu/announcement.svg?svgr";
+import CloseClubIcon from "../images/icon/clubMenu/close_club.svg?svgr";
+import HelpIcon from "../images/icon/clubMenu/help.svg?svgr";
+import MembersIcon from "../images/icon/clubMenu/members.svg?svgr";
+import NotificationIcon from "../images/icon/clubMenu/notification.svg?svgr";
+import RecruitAnnouncementIcon from "../images/icon/clubMenu/recruit_announcement.svg?svgr";
+import RecruitManagementIcon from "../images/icon/clubMenu/recruit_management.svg?svgr";
+import ScheduleIcon from "../images/icon/clubMenu/schedule.svg?svgr";
+import WithdrawalIcon from "../images/icon/clubMenu/withdrawal.svg?svgr";
+
 // 권한에 따른 메뉴 목록
 export const CLUB_MENU_MAP = {
   ADMIN: CLUB_LEFT_MENU_ADMIN,
@@ -19,4 +35,24 @@ export const CLUB_MENU_ROLE_LABELS = {
   ADMIN: "관리자",
   MANAGER: "관리자",
   GENERAL: "일반회원",
+};
+
+export const MENU_ICONS: Record<
+  string,
+  React.FC<React.SVGProps<SVGSVGElement>>
+> = {
+  acceptance_review: AcceptanceReviewIcon,
+  accounting: AccountingIcon,
+  activity_review: ActivityReviewIcon,
+  activity_history: ActivityHistoryIcon,
+  activity_management: ActivityManagementIcon,
+  announcement: AnnouncementIcon,
+  close_club: CloseClubIcon,
+  help: HelpIcon,
+  members: MembersIcon,
+  notification: NotificationIcon,
+  recruit_announcement: RecruitAnnouncementIcon,
+  recruit_management: RecruitManagementIcon,
+  schedule: ScheduleIcon,
+  withdrawal: WithdrawalIcon,
 };
