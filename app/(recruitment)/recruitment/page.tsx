ae2b4cbe19a7119d0bbeaf1b9a28563d87ebfeff
yeoniii20 <100350818+yeoniii20@users.noreply.github.com>
2025-04-01 17:39:25 +0900

    feat: Apply filter

--- app/(recruitment)/recruitment/page.tsx
+++ app/(recruitment)/recruitment/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import PlusBtn from "@/components/button/withIconBtn/plusBtn";
 import FilterSection from "./content/filterSection";
 import MainRecruitmentCard from "@/components/card/mainRecruitmentCard";
 import HeaderSection from "./content/headerSection";
 import { POPULARITY_SORT_TYPE } from "@/data/pulldown";
-import { useEffect, useState } from "react";
 import { RecruitmentData, RecruitmentResponse } from "@/types/recruitment";
 import { ClubSearchCondition, Pageable } from "@/types/api";
 import { getAllRecruitments } from "@/api/recruitment/api";
@@ -18,19 +18,25 @@ const Recruitment = () => {
     POPULARITY_SORT_TYPE[0].label
   );
 
-  // 모집 목록
+  const [affiliationFilter, setAffiliationFilter] = useState<string | null>(
+    null
+  );
+  const [fieldFilter, setFieldFilter] = useState<string | null>(null);
+  const [areaFilter, setAreaFilter] = useState<string | null>(null);
+  const [targetFilter, setTargetFilter] = useState<string | null>(null);
+
   const fetchRecruitments = async (newPage = 0, reset = false) => {
     try {
       const condition: ClubSearchCondition = {
-        clubCategoryTypes: undefined,
-        clubRegionTypes: undefined,
-        participantTypes: undefined,
+        clubCategoryTypes: affiliationFilter ? [affiliationFilter] : undefined,
+        clubRegionTypes: areaFilter ? [areaFilter] : undefined,
+        participantTypes: targetFilter ? [targetFilter] : undefined,
       };
 
       const pageable: Pageable = {
         page: newPage,
         size: 10,
-        sort: [""],
+        sort: sortType === "정렬 기준" ? [] : [sortType],
       };
 
       const response: RecruitmentResponse = await getAllRecruitments(
@@ -40,7 +46,6 @@ const Recruitment = () => {
 
       if (reset) {
         setRecruitmentData(response.recruitmentDataList);
-        console.log("모집 리스트 조회 >>>", response);
       } else {
         setRecruitmentData((prev) => [
           ...prev,
@@ -57,13 +62,30 @@ const Recruitment = () => {
   };
 
   useEffect(() => {
+    console.log("렌더링 >>>");
     fetchRecruitments(0, true);
-  }, []);
+    // }, []);
+  }, [affiliationFilter, fieldFilter, areaFilter, targetFilter, sortType]);
+
+  useEffect(() => {
+    console.log("필터 상태:", {
+      affiliationFilter,
+      fieldFilter,
+      areaFilter,
+      targetFilter,
+    });
+  }, [affiliationFilter, fieldFilter, areaFilter, targetFilter]);
 
   return (
     <div>
       <div className="flex flex-col gap-5 mt-5 md:mt-8 md:gap-[22px]">
-        <HeaderSection />
+        <HeaderSection
+          setAffiliationFilter={setAffiliationFilter}
+          setFieldFilter={setFieldFilter}
+          setAreaFilter={setAreaFilter}
+          setTargetFilter={setTargetFilter}
+          setSortType={setSortType}
+        />
         <FilterSection
           sortType={sortType}
           setSortType={setSortType}
@@ -71,16 +93,13 @@ const Recruitment = () => {
         />
       </div>
       <div className="flex flex-col mt-5 md:mt-7">
-        <div
-          className="md:min-h-[450px] min-h-[300px] flex flex-col gap-5 md:gap-x-4 md:gap-y-7 md:flex-row md:flex-wrap
-          md:grid md:grid-cols-3 lx:grid-cols-4"
-        >
+        <div className="md:min-h-[450px] min-h-[300px] flex flex-col gap-5 md:gap-x-4 md:gap-y-7 md:flex-row md:flex-wrap md:grid md:grid-cols-3 lx:grid-cols-4">
           <MainRecruitmentCard data={recruitmentData} />
         </div>
         <div className="self-center mt-9 mb-[80px] md:mt-10 md:mb-[124px]">
           {more && (
             <PlusBtn
-              title={"더보기"}
+              title="더보기"
               onClick={() => {
                 const nextPage = page + 1;
                 setPage(nextPage);
