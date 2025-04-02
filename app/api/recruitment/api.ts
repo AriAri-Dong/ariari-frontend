e7146aab33c9c8fd73d8333e6a4102221086395d
yeoniii20 <100350818+yeoniii20@users.noreply.github.com>
2025-04-02 12:49:09 +0900

    feat: Update recruitment api.ts

--- app/api/recruitment/api.ts
+++ app/api/recruitment/api.ts
@@ -2,27 +2,38 @@ import axios from "axios";
 import {
   RECRUITMENT,
   RECRUITMENT_BOOKMARKS,
-  RECRUITMENT_RANKING,
+  RECRUITMENT_EXTERNAL,
+  RECRUITMENT_INTERNAL,
+  RECRUITMENT_RANKING_EXTERNAL,
+  RECRUITMENT_RANKING_INTERNAL,
 } from "../apiUrl";
 import axiosInstance from "../axiosInstance";
 import { ClubSearchCondition, Pageable } from "@/types/api";
-import { RecruitmentData, RecruitmentResponse } from "@/types/recruitment";
+import { RecruitmentResponse } from "@/types/recruitment";
 
-export const getRecruitmentRanking = async () => {
+// 교내 모집 랭킹 리스트 조회
+export const getInternalRecruitmentRanking = async () => {
   try {
-    const { data } = await axios.get(RECRUITMENT_RANKING);
+    const { data } = await axiosInstance.get(RECRUITMENT_RANKING_INTERNAL);
     console.log("성공 >>>", data);
     return data;
   } catch (err) {
     console.error("조회 실패:", err);
-    // return {
-    //   memberData: { id: "", nickname: "", profileType: "" },
-    //   schoolData: { name: "" },
-    // };
   }
 };
 
-// 전체 모집리스트 조회
+// 교외 모집 랭킹 리스트 조회
+export const getExternalRecruitmentRanking = async () => {
+  try {
+    const { data } = await axios.get(RECRUITMENT_RANKING_EXTERNAL);
+    console.log("성공 >>>", data);
+    return data;
+  } catch (err) {
+    console.error("조회 실패:", err);
+  }
+};
+
+// 전체 모집 리스트 조회
 export const getAllRecruitments = async (
   condition: ClubSearchCondition,
   pageable: Pageable
@@ -51,6 +62,86 @@ export const getAllRecruitments = async (
     const response = await axiosInstance.get<RecruitmentResponse>(RECRUITMENT, {
       params,
     });
+
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching recruitment info:", error);
+    throw error;
+  }
+};
+
+// 교내 모집 리스트 조회
+export const getInternalRecruitments = async (
+  condition: ClubSearchCondition,
+  pageable: Pageable
+): Promise<RecruitmentResponse> => {
+  try {
+    const filteredCondition = {
+      clubCategoryTypes: condition.clubCategoryTypes
+        ? condition.clubCategoryTypes[0]
+        : undefined,
+      clubRegionTypes: condition.clubRegionTypes
+        ? condition.clubRegionTypes[0]
+        : undefined,
+      participantTypes: condition.participantTypes
+        ? condition.participantTypes[0]
+        : undefined,
+    };
+
+    // 쿼리 파라미터 생성
+    const params = {
+      ...filteredCondition,
+      page: pageable.page,
+      size: pageable.size,
+      ...(pageable.sort ? { sort: pageable.sort.join(",") } : {}),
+    };
+
+    const response = await axiosInstance.get<RecruitmentResponse>(
+      RECRUITMENT_INTERNAL,
+      {
+        params,
+      }
+    );
+
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching recruitment info:", error);
+    throw error;
+  }
+};
+
+// 교외 모집 리스트 조회
+export const getExternalRecruitments = async (
+  condition: ClubSearchCondition,
+  pageable: Pageable
+): Promise<RecruitmentResponse> => {
+  try {
+    const filteredCondition = {
+      clubCategoryTypes: condition.clubCategoryTypes
+        ? condition.clubCategoryTypes[0]
+        : undefined,
+      clubRegionTypes: condition.clubRegionTypes
+        ? condition.clubRegionTypes[0]
+        : undefined,
+      participantTypes: condition.participantTypes
+        ? condition.participantTypes[0]
+        : undefined,
+    };
+
+    // 쿼리 파라미터 생성
+    const params = {
+      ...filteredCondition,
+      page: pageable.page,
+      size: pageable.size,
+      ...(pageable.sort ? { sort: pageable.sort.join(",") } : {}),
+    };
+
+    const response = await axiosInstance.get<RecruitmentResponse>(
+      RECRUITMENT_EXTERNAL,
+      {
+        params,
+      }
+    );
 
     return response.data;
   } catch (error) {
