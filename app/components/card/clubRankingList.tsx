fc3f257bdb28ea7243ae545bf3e23ad5936df9ae
yeoniii20 <100350818+yeoniii20@users.noreply.github.com>
2025-04-02 14:08:43 +0900

    feat: Modify clubs type

--- app/components/card/clubRankingList.tsx
+++ app/components/card/clubRankingList.tsx
@@ -1,19 +1,11 @@
 "use client";
 
 import Image from "next/image";
-import no_image from "@/images/noImage/no-image.jpg";
-import useResponsive from "@/hooks/useResponsive";
-
-interface Club {
-  id: number;
-  title: string;
-  description: string;
-  rank: number;
-  imageSrc?: string;
-}
+import { ClubData } from "@/types/club";
+import defaultImg from "@/images/icon/defaultAriari.svg";
 
 interface ClubRankingListProps {
-  clubs: Club[];
+  clubs: ClubData[];
 }
 
 const ClubRankingList = ({ clubs }: ClubRankingListProps) => {
@@ -22,7 +14,7 @@ const ClubRankingList = ({ clubs }: ClubRankingListProps) => {
       <div
         className={`grid gap-4 grid-cols-[repeat(3,minmax(204px,1fr))] md:grid-cols-[repeat(2,minmax(204px,1fr))] lg:grid-cols-[repeat(3,minmax(204px,1fr))]`}
       >
-        {clubs.map((club) => (
+        {clubs.map((club, index) => (
           <div
             key={club.id}
             className="min-w-[204px] rounded-lg flex-shrink-0 cursor-pointer "
@@ -30,20 +22,20 @@ const ClubRankingList = ({ clubs }: ClubRankingListProps) => {
             <div className="flex items-center gap-[18px] md:gap-[30px]">
               <div className="flex items-center gap-[14px] md:gap-[26px]">
                 <Image
-                  src={club.imageSrc || no_image}
-                  alt={club.title}
+                  src={club.profileUri || defaultImg}
+                  alt={club.name}
                   className="w-[56px] h-[56px] rounded-full object-cover md:w-[98px] md:h-[98px]"
                 />
                 <div className="text-base font-semibold md:text-2xl">
-                  {club.rank}
+                  {index + 4}
                 </div>
               </div>
               <div className="flex-1 overflow-hidden">
                 <div className="text-mobile_h4_sb truncate mb-1 md:text-h3 md:mb-2">
-                  {club.title}
+                  {club.name}
                 </div>
                 <p className="w-[80%] text-mobile_body3_r truncate text-subtext2 md:text-body1_r">
-                  {club.description}
+                  {club.body}
                 </p>
               </div>
             </div>
