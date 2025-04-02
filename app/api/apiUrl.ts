16ccffbaa12702c4b97a8c7ab2ba02e3875d63b2
yeoniii20 <100350818+yeoniii20@users.noreply.github.com>
2025-04-02 14:31:25 +0900

    feat: Update apiUrl.ts

--- app/api/apiUrl.ts
+++ app/api/apiUrl.ts
@@ -38,6 +38,7 @@ export const TEST_ADMIN = "/test/auth/ADMIN";
 export const SCHOOLS = "/schools";
 export const SCHOOLS_VALIDATE = "/schools/auth/validate";
 export const SCHOOLS_SEND = "/schools/auth/send";
+export const SCHOOLS_CANCEL = "/schools/auth/cancel";
 
 // == APPLY_TEMPS ==
 export const APPLY_TEMPS = "/apply-temp";
