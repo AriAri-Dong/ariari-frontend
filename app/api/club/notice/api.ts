35e8af693a6c383237bd590bb9810d9e31bd1881
seyoun <lsy20140@gmail.com>
2025-04-02 15:13:05 +0900

    feat: Add getClubNoticeList api and custom query hook

---
+++ app/api/club/notice/api.ts
@@ -0,0 +1,20 @@
+import { CLUBS } from "@/api/apiUrl";
+import axiosInstance from "@/api/axiosInstance";
+import { ClubNoticeDataRes } from "@/types/club";
+
+/**
+ * 상단에 고정된 동아리 공지사항 리스트 조회
+ * @param clubId
+ * @returns 고정된 공지사항 리스트
+ */
+export const getClubFixedNoticeList = async (clubId: string) => {
+  try {
+    const res = await axiosInstance.get<ClubNoticeDataRes>(
+      `${CLUBS}/${clubId}/club-notices/fix`
+    );
+    return res.data;
+  } catch (error) {
+    console.log("Failed to fetch fixed club notices", error);
+    throw error;
+  }
+};
---
+++ app/hooks/club/useClubNoticeQuery.tsx
@@ -0,0 +1,17 @@
+import { getClubFixedNoticeList } from "@/api/club/notice/api";
+import { useQuery } from "@tanstack/react-query";
+
+// 고정된 동아리 공지사항 리스트 조회
+export const useClubPinnedNoticeQuery = (clubId: string) => {
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["club", clubId, "pinned-notices"],
+    queryFn: () => getClubFixedNoticeList(clubId),
+  });
+
+  return {
+    pinnedNoticeList: data?.clubNoticeDataList || [],
+    isLoading,
+    isError,
+    error,
+  };
+};
