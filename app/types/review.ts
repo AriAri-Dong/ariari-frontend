37fb0a645f075e5f67c77bef993dd4b94f457e63
bsu0404 <bsu0404@naver.com>
2025-04-06 23:57:26 +0900

    feat: Add passreview related types

--- app/types/review.ts
+++ app/types/review.ts
@@ -1,5 +1,7 @@
 import { PageInfo } from "./pageInfo";
 
+// --------- 활동 후기 -----------
+
 export interface TagData {
   id: string;
   body: string;
@@ -27,7 +29,7 @@ export interface ClubReviewDetail {
   tagDataList: TagData[];
 }
 
-// 활동후기 리스트
+// 활동후기 리스트 정보
 export interface ClubReviewData {
   id: string;
   title: string;
@@ -44,4 +46,61 @@ export interface ClubReviewSaveReq {
   title: string;
   body: string;
   icons: TagIconType[];
+}
+
+// --------- 합격후기 -----------
+
+export type ProcedureType = "DOCUMENT" | "INTERVIEW";
+export type InterviewType = "ONLINE" | "OFFLINE" | "CALL";
+export type InterviewRatioType = "ONE_VS_ONE" | "ONE_VS_MANY" | "MANY_VS_MANY";
+
+// 합격후기 리스트 정보
+export interface PassReviewData {
+  id: string;
+  title: string;
+  createdDateTime: string;
+  documentNoteCount: number;
+  interviewNoteCount: number;
+}
+
+// 합격후기 리스트 res
+export interface PassReviewListRes {
+  contents: PassReviewData[];
+  pageInfo: PageInfo;
+}
+
+// 질문 답변
+export interface PassReviewNoteData {
+  title: string;
+  body: string;
+}
+
+// 합격후기 상세 정보
+export interface PassReviewDetail {
+  id: string;
+  creatorId: string;
+  procedureType: ProcedureType;
+  interviewType: InterviewType;
+  interviewRatioType: InterviewRatioType;
+  interviewMood: number;
+  documentNotes: PassReviewNoteData[];
+  interviewNotes: PassReviewNoteData[];
+  createdDateTime: string;
+}
+
+// 질문 답변 타입
+export interface PassReviewNote {
+  title: string;
+  body: string;
+  noteType: ProcedureType;
+}
+
+// 합격후기 작성 req
+export interface PassReviewSaveReq {
+  title: string;
+  procedureType: ProcedureType;
+  interviewType: InterviewType;
+  interviewRatioType: InterviewRatioType;
+  interviewMood: number;
+  passReviewNotes: PassReviewNote[];
 }
