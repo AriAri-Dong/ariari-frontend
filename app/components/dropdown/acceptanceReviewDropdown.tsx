070fedc52473e1067b605aa4494bc512b230bd68
bsu0404 <bsu0404@naver.com>
2025-04-07 16:54:41 +0900

    feat: Connect review detail API

--- app/components/dropdown/acceptanceReviewDropdown.tsx
+++ app/components/dropdown/acceptanceReviewDropdown.tsx
@@ -1,71 +1,71 @@
 "use client";
 
 import React, { useState } from "react";
+import { useUserStore } from "@/providers/user-store-provider";
+import { useShallow } from "zustand/shallow";
+import useResponsive from "@/hooks/useResponsive";
 import TransparentSmallBtn from "../button/basicBtn/transparentSmallBtn";
 import IconBadge from "../badge/iconBadge";
 import IconBtn from "../button/withIconBtn/IconBtn";
 import ReportModal from "../modal/reportModal";
 import Alert from "../alert/alert";
 import ProgressBar from "../bar/progressBar";
-import CustomInput from "../input/customInput";
+import { PassReviewData, PassReviewDetail } from "@/types/review";
+import ReportBottomSheet from "../bottomSheet/report/reportBottomSheet";
+import formatDateToDot from "@/utils/formatDateToDot";
+import { getAccectanceReviewDetail } from "@/api/review/api";
+import ReviewField from "../input/reviewField";
+import {
+  getInterviewRatioLabel,
+  getInterviewTypeLabel,
+  getProcedureTypeLabel,
+} from "@/utils/reviewMapping";
 
 interface AcceptanceReviewDropdownProps {
   onClick: () => void;
-  onBtnClick: () => void;
-  title: string;
-  date: string;
-  document: number;
-  interview: number;
-  isOpen: boolean;
-  onToggle: () => void;
+  review: PassReviewData;
+  isOpenReview: boolean;
 }
-
-const SECTIONS = [
-  {
-    title: "서류문항",
-    subDescription: "문항",
-    questions: [
-      { id: 1, question: "작성된 문항 1", answer: "작성된 답변 1" },
-      { id: 2, question: "작성된 문항 2", answer: "작성된 답변 2" },
-    ],
-  },
-  {
-    title: "면접질문",
-    subDescription: "질문",
-    questions: [
-      { id: 1, question: "작성된 질문 1", answer: "작성된 답변 1" },
-      { id: 2, question: "작성된 질문 2", answer: "작성된 답변 2" },
-      { id: 3, question: "작성된 질문 3", answer: "작성된 답변 3" },
-    ],
-  },
-];
 
 const AcceptanceReviewDropdown = ({
   onClick,
-  onBtnClick,
-  title,
-  date,
-  document,
-  interview,
-  isOpen,
-  onToggle,
+  review,
+  isOpenReview,
 }: AcceptanceReviewDropdownProps) => {
-  const [inputValue, setInputValue] = useState<string>("");
-  const [isBottomModalOpen, setIsBottomModalOpen] = useState<boolean>(false);
+  const isMdUp = useResponsive("md");
+  const memberId = useUserStore(useShallow((state) => state.memberData.id));
+
+  const [reviewDetail, setReviewDetail] = useState<PassReviewDetail | null>(
+    null
+  );
+  const [isReportOpen, setIsReportOpen] = useState<boolean>(false);
   const [alertMessage, setAlertMessage] = useState<string | null>(null);
 
+  const [isInterview, setIsInterview] = useState<boolean>(false);
+
   const toggleBottomModal = () => {
-    setIsBottomModalOpen((prev) => !prev);
+    setIsReportOpen((prev) => !prev);
   };
 
   const handleReportSubmit = () => {
-    setIsBottomModalOpen(false);
+    setIsReportOpen(false);
     setAlertMessage("신고가 정상적으로 접수되었습니다.");
+  };
+
+  const handleToggleDropdown = () => {
+    onClick();
+
+    if (!reviewDetail) {
+      getAccectanceReviewDetail(review.id).then((detail) => {
+        setReviewDetail(detail);
+        setIsInterview(detail.procedureType === "INTERVIEW");
+      });
+    }
   };
 
   return (
     <div
-      className="w-full max-w-[1248px] p-4 md:pt-[18px] md:pb-[22px] md:pl-9 md:pr-[34px]
+      className="w-full max-w-[1248px] p-4 md:py-[26px] md:px-6 
     rounded-lg bg-background cursor-pointer"
     >
       {/* 기본 내용 */}
@@ -73,21 +73,31 @@ const AcceptanceReviewDropdown = ({
         <div className="flex flex-col w-full items-start md:flex-row gap-3 md:gap-12">
           <div className="flex flex-col w-full gap-4 md:gap-5">
             <div className="flex items-center justify-between">
-              <h3 className="text-body1_m md:text-h4_sb text-text1">{title}</h3>
+              <h3 className="text-body1_m md:text-h4_sb text-text1">
+                {review.title}
+              </h3>
               <div className="md:flex hidden sm:justify-between sm:items-center sm:w-full md:w-auto md:self-startmd:m-w-[194px] md:gap-7">
                 <p className="text-subtext2 text-body4_r content-center md:text-body3_r">
-                  {date}
+                  {formatDateToDot(review.createdDateTime)}
                 </p>
                 <TransparentSmallBtn
                   title={"열람하기"}
-                  onClick={onToggle}
+                  onClick={handleToggleDropdown}
                   round={true}
                 />
               </div>
             </div>
             <div className="flex flex-col gap-2 md:flex-row md:gap-[44px]">
-              <IconBadge type={"서류"} text={`답변 문항 ${document}개`} />
-              <IconBadge type={"면접"} text={`답변 문항 ${interview}개`} />
+              <IconBadge
+                type={"서류"}
+                text={`답변 문항 ${review.documentNoteCount}개`}
+              />
+              {review.interviewNoteCount !== 0 && (
+                <IconBadge
+                  type={"면접"}
+                  text={`답변 문항 ${review.interviewNoteCount}개`}
+                />
+              )}
             </div>
           </div>
         </div>
@@ -96,11 +106,11 @@ const AcceptanceReviewDropdown = ({
         md:hidden"
         >
           <p className="text-subtext2 text-body4_r content-center md:text-body3_r">
-            {date}
+            {formatDateToDot(review.createdDateTime)}
           </p>
           <TransparentSmallBtn
             title={"열람하기"}
-            onClick={onToggle}
+            onClick={handleToggleDropdown}
             round={true}
           />
         </div>
@@ -109,98 +119,116 @@ const AcceptanceReviewDropdown = ({
       {/* 드롭다운 */}
       <div
         className={`transition-all duration-300 ease-in-out overflow-hidden ${
-          isOpen ? "opacity-100" : "max-h-0 opacity-0"
+          isOpenReview && reviewDetail ? "opacity-100" : "max-h-0 opacity-0"
         }`}
       >
-        <div className="flex flex-col md:flex-row w-full justify-between mb-4 mt-7 md:mt-9 md:mb-2 md: gap-[56px]">
-          <div className="flex md:flex-col w-full justify-between md:justify-normal md:w-[183px] md:gap-[44px]">
-            {["합격전형", "면접방식", "면접인원"].map((label, index) => (
-              <div key={index} className="flex flex-col gap-2.5 md:gap-[14px]">
+        <div className="flex flex-col md:flex-row w-full justify-between mb-4 mt-7 md:mt-9 md:mb-2 md:gap-[56px]">
+          <div className="grid grid-cols-3 md:grid-cols-1 w-full justify-between md:justify-normal md:w-[183px] md:gap-[44px]">
+            {(isInterview
+              ? ["합격전형", "면접방식", "면접인원"]
+              : ["합격전형"]
+            ).map((label, index) => (
+              <div
+                key={index}
+                className={`flex flex-col gap-2.5 md:gap-[14px]
+                  ${index === 1 && "items-center"}
+                  ${index === 2 && "items-end"}
+                  md:items-start`}
+              >
                 <h3 className="text-mobile_body1_sb md:text-h4_sb text-text1">
                   {label}
                 </h3>
                 <p className="text-subtext2 text-mobile_body1_r md:text-body2_m">
                   {index === 0
-                    ? "서류 · 면접 전형"
+                    ? getProcedureTypeLabel(reviewDetail?.procedureType)
                     : index === 1
-                    ? "온라인"
-                    : "그룹면접"}
+                    ? getInterviewRatioLabel(reviewDetail?.interviewRatioType)
+                    : getInterviewTypeLabel(reviewDetail?.interviewType)}
                 </p>
               </div>
             ))}
+
             {/* PC */}
-            <div className="hidden md:flex flex-col md:gap-[14px] mb-6">
+            {isInterview && (
+              <div className="hidden md:flex flex-col md:gap-[14px] mb-6">
+                <h3 className="text-mobile_body1_sb md:text-h4_sb text-text1">
+                  면접분위기
+                </h3>
+                <ProgressBar
+                  disabled={true}
+                  currentStep={reviewDetail?.interviewMood}
+                />
+              </div>
+            )}
+          </div>
+          {/* 모바일 */}
+          {isInterview && (
+            <div className="flex md:hidden flex-col gap-2.5 mt-[26px]">
               <h3 className="text-mobile_body1_sb md:text-h4_sb text-text1">
                 면접분위기
               </h3>
-              <ProgressBar disabled={true} initialStep={4} />
+              <ProgressBar
+                disabled={true}
+                currentStep={reviewDetail?.interviewMood}
+              />
             </div>
-          </div>
-          {/* 모바일 */}
-          <div className="flex md:hidden flex-col gap-2.5 mb-8 mt-[26px]">
-            <h3 className="text-mobile_body1_sb md:text-h4_sb text-text1">
-              면접분위기
-            </h3>
-            <ProgressBar disabled={true} initialStep={2} />
-          </div>
-          <div className="flex flex-col w-full  md:gap-10 md:max-h-none">
-            {SECTIONS.map((section, sectionIdx) => (
-              <div key={sectionIdx}>
+          )}
+          <div className="flex flex-col w-full gap-7 mt-8 md:mt-0 md:gap-10 md:max-h-none">
+            <div>
+              <h3 className="md:block hidden text-body1_m md:text-h4_sb mb-6 text-text1">
+                서류 문항
+              </h3>
+              <div className="flex flex-col gap-6 md:max-h-[380px] md:overflow-y-auto no-scrollbar">
+                {reviewDetail?.documentNotes.map((note, idx) => (
+                  <div key={idx}>
+                    <ReviewField note={note} title={"문항"} idx={idx + 1} />
+                  </div>
+                ))}
+              </div>
+            </div>
+            {isInterview && (
+              <div>
                 <h3 className="md:block hidden text-body1_m md:text-h4_sb mb-6 text-text1">
-                  {section.title}
+                  면접 질문
                 </h3>
                 <div className="flex flex-col md:max-h-[380px] md:overflow-y-auto no-scrollbar">
-                  {section.questions.map((question, questionIdx) => (
-                    <div
-                      key={questionIdx}
-                      className={`${questionIdx === 0 ? "" : "mt-6"}`}
-                    >
-                      <p
-                        className={`md:text-h4 text-mobile_body1_sb text-text1 mb-4 md:mb-[18px]
-                          ${
-                            section.subDescription === "질문" &&
-                            question.id === 1
-                              ? "md:mt-0 mt-7"
-                              : ""
-                          }`}
-                      >
-                        {section.subDescription}-{question.id}
-                      </p>
-                      <div className="flex flex-col gap-[14px]">
-                        <CustomInput
-                          value={question.question}
-                          placeholder="작성된 문항"
-                          disable={true}
-                          onChange={() => {}}
-                        />
-                        <CustomInput
-                          value={question.answer}
-                          placeholder="작성된 답변"
-                          disable={true}
-                          onChange={() => {}}
-                        />
-                      </div>
+                  {reviewDetail?.interviewNotes.map((note, idx) => (
+                    <div key={idx}>
+                      <ReviewField note={note} title={"질문"} idx={idx + 1} />
                     </div>
                   ))}
                 </div>
               </div>
-            ))}
+            )}
           </div>
         </div>
-        <IconBtn
-          type={"declaration"}
-          size={"large"}
-          title={"신고하기"}
-          onClick={toggleBottomModal}
-        />
+        {reviewDetail?.creatorId !== memberId && (
+          <IconBtn
+            type={"declaration"}
+            size={"large"}
+            title={"신고하기"}
+            onClick={toggleBottomModal}
+          />
+        )}
       </div>
 
-      {isBottomModalOpen && (
-        <ReportModal
-          onClose={() => setIsBottomModalOpen(false)}
-          onSubmit={handleReportSubmit}
-        />
-      )}
+      {isMdUp
+        ? isReportOpen && (
+            <ReportModal
+              id={review.id}
+              reportTargetType="PASS_REVIEW"
+              onClose={() => setIsReportOpen(false)}
+              onSubmit={handleReportSubmit}
+            />
+          )
+        : isReportOpen && (
+            <ReportBottomSheet
+              id={review.id}
+              reportTargetType="PASS_REVIEW"
+              onClose={() => setIsReportOpen(false)}
+              onSubmit={handleReportSubmit}
+            />
+          )}
       {alertMessage && (
         <Alert text={alertMessage} onClose={() => setAlertMessage(null)} />
       )}
