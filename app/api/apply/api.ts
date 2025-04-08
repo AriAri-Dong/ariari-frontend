61470927f421bda25f62997003b96e30bab47866
bsu0404 <bsu0404@naver.com>
2025-04-09 04:54:39 +0900

    feat: Add getApplyForm api

--- app/api/apply/api.ts
+++ app/api/apply/api.ts
@@ -1,5 +1,7 @@
+import { AxiosError } from "axios";
 import { APPLY_TEMPS_MY } from "../apiUrl";
 import axiosInstance from "../axiosInstance";
+import { ApplyFormData } from "@/types/application";
 
 export const getAppliedList = async (
   page: number = 0,
@@ -26,5 +28,22 @@ export const getAppliedList = async (
     return {
       data: [],
     };
+  }
+};
+
+// 지원형식 조회
+export const getApplyForm = async (clubId: string) => {
+  try {
+    const response = await axiosInstance.get<ApplyFormData>(
+      `/clubs/${clubId}/apply-forms`
+    );
+    return response.data;
+  } catch (err) {
+    if (err instanceof AxiosError) {
+      if (err.response && err.response.data.message) {
+        throw new Error(err.response.data.message);
+      }
+    }
+    throw new Error("문제가 발생했습니다.");
   }
 };
