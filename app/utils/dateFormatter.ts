ddf428e89f3d7370f465953d121085e70c949370
yeoniii20 <100350818+yeoniii20@users.noreply.github.com>
2025-04-09 10:45:37 +0900

    feat: Update dateFormatter.ts

--- app/utils/dateFormatter.ts
+++ app/utils/dateFormatter.ts
@@ -1,6 +1,11 @@
 export const calculateRemainingDays = (date: string): string => {
+  if (!date) return "마감";
+
   const today = new Date();
   const targetDate = new Date(date);
+
+  if (isNaN(targetDate.getTime())) return "마감";
+
   const differenceInTime = targetDate.getTime() - today.getTime();
   const remainingDays = Math.ceil(differenceInTime / (1000 * 3600 * 24));
 
