7d2fe3ef79027fa07bbd2caa4294f2ccf5c2fec3
Seyoun <lsy20140@gmail.com>
2025-04-12 19:02:28 +0900

    refactor: Add NotificationItemWrapper component

--- app/components/list/notificationList.tsx
+++ app/components/list/notificationList.tsx
@@ -1,62 +1,91 @@
-import useResponsive from "@/hooks/useResponsive";
 import rightArrow from "@/images/icon/vector.svg";
-import {
-  ClubNotificationData,
-  MemberNotificationData,
-} from "@/types/notification";
+import { NotificationData } from "@/types/notification";
+
 import formatDateToDot, { formatTime } from "@/utils/formatDateToDot";
 import Image from "next/image";
+import Link from "next/link";
 
 interface NotificationListProps {
-  notificationList: (ClubNotificationData | MemberNotificationData)[];
+  notificationList: NotificationData[];
   className?: string;
 }
 
+// 알림 목록
 const NotificationList = ({
   notificationList,
   className,
 }: NotificationListProps) => {
-  const isMdUp = useResponsive("md");
-
   if (!notificationList.length) {
     return <p className="text-center">새로운 알림이 없습니다.</p>;
   }
-
   return (
     <ul>
       {notificationList.map((item, index) => {
-        const { id, title, isChecked, createdDateTime } = item;
-
+        const { id, isChecked, title, uri, createdDateTime } = item;
         return (
           <li
             key={id}
-            className={`flex items-center justify-between py-[14px] px-2.5 cursor-pointer border-b last:border-b-0 ${
-              isChecked && "opacity-70"
-            } ${className}`}
+            className={`py-[14px] px-2.5 border-b last:border-b-0 ${className}`}
           >
-            <div className="flex flex-col gap-2 md:gap-[6px]">
-              <h3
-                className={`text-mobile_body1_m md:text-body2_m ${
-                  isChecked ? "text-subtext2" : "text-text1"
-                }`}
-              >
-                {title}
-              </h3>
-              <div className="flex md:gap-[10px] gap-2 text-unselected text-mobile_body4_r md:text-body4_r">
-                <p>{formatDateToDot(createdDateTime, false)}</p>
-                <p>{formatTime(new Date(createdDateTime))}</p>
+            <NotificationItemWrapper item={item}>
+              <div className="w-full h-[45px] flex flex-col gap-2 text-left md:gap-[6px]">
+                <h3
+                  className={`text-mobile_body1_m md:text-body2_m ${
+                    isChecked ? "text-subtext2" : "text-text1"
+                  }`}
+                >
+                  {title}
+                </h3>
+                <div className="flex md:gap-[10px] gap-2 text-unselected text-mobile_body4_r md:text-body4_r">
+                  <p>{formatDateToDot(createdDateTime, false)}</p>
+                  <p>{formatTime(new Date(createdDateTime))}</p>
+                </div>
               </div>
-            </div>
-            <Image
-              src={rightArrow}
-              alt={"알림 바로가기"}
-              width={isMdUp ? 24 : 20}
-              height={isMdUp ? 24 : 20}
-            />
+            </NotificationItemWrapper>
           </li>
         );
       })}
     </ul>
+  );
+};
+
+// uri 존재 여부에 따른 상위 wrapper
+const NotificationItemWrapper = ({
+  item,
+  children,
+  className,
+}: {
+  item: NotificationData;
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  const { uri, isChecked } = item;
+
+  const baseClassName = "w-full flex items-center justify-between gap-3";
+  const newClassName = `${baseClassName} ${className ?? ""} ${
+    isChecked ? "opacity-70" : ""
+  }`;
+
+  if (uri) {
+    return (
+      <Link href={uri} className={newClassName}>
+        {children}
+        <Image
+          src={rightArrow}
+          alt={"알림 바로가기"}
+          className="w-5 h-5 md:w-6 md:h-6"
+        />
+      </Link>
+    );
+  }
+
+  return (
+    <button
+      onClick={() => console.log("개별 알림 항목 클릭")}
+      className={newClassName}
+    >
+      {children}
+    </button>
   );
 };
 
