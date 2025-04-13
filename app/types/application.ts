cb4d608482eb3ed1dd79ca44ce5b5adf6bd9aee6
bsu0404 <bsu0404@naver.com>
2025-04-13 10:42:51 +0900

    feat: Add types for temp application

--- app/types/application.ts
+++ app/types/application.ts
@@ -1,3 +1,39 @@
+import { ClubData } from "./club";
+import { MemberData } from "./member";
+import { PageInfo } from "./pageInfo";
+import { RecruitmentData } from "./recruitment";
+
+export type ApplyStatusType = "PENDENCY" | "APPROVE" | "REFUSAL" | "INTERVIEW";
+
+export interface ApplyData {
+  id: string;
+  name: string;
+  applyStatusType: ApplyStatusType;
+  createdDateTime: string;
+  memberData: MemberData;
+  recruitmentTitle: string;
+  clubName: string;
+}
+
+export interface ApplyListRes {
+  applyDataList: ApplyData[];
+  pageInfo: PageInfo;
+}
+
+export interface ApplyTempData {
+  id: string;
+  name: string;
+  createdDateTime: string;
+  recruitmentTitle: string;
+  clubName: string;
+  recruitmentId: string;
+}
+
+export interface ApplyTempListRes {
+  applyDataList: ApplyTempData[];
+  pageInfo: PageInfo;
+}
+
 export type ApplicationKeys =
   | "gender"
   | "birthday"
@@ -22,16 +58,16 @@ export type SpecialQuestionList = {
   [key in ApplicationKeys]: string | null;
 };
 
-export type ApplyQuestionData = {
+export interface ApplyQuestionData {
   id: string;
   body: string;
-};
+}
 
-export type ApplyFormData = {
+export interface ApplyFormData {
   specialQuestionList: SpecialQuestionList;
   applyQuestionDataList: ApplyQuestionData[];
   portfolio: boolean;
-};
+}
 
 export type ApplyField =
   | { key: ApplicationKeys; name: string; type: "radio"; options: string[] }
@@ -51,17 +87,32 @@ export type ApplyField =
     }
   | { key: ApplicationKeys; name: string; type: "date" };
 
-export type ApplyFormRes = {
+export interface ApplyFormRes {
   applyFormData: ApplyFormData;
-};
+}
 
-export type ApplyAnswerReq = {
+export interface ApplyAnswerReq {
   applyQuestionId: string;
   body: string;
-};
+}
 
-export type ApplySaveReq = {
+export interface ApplySaveReq {
   name: string;
   portfolioUrl: string;
   applyAnswers: ApplyAnswerReq[];
-};
+}
+
+export interface ApplyAnswerTempData {
+  body: string;
+  applyQuestionData: ApplyQuestionData;
+}
+
+export interface ApplyTempDetailRes {
+  applyData: ApplyTempData;
+  clubData: ClubData;
+  recruitmentData: RecruitmentData;
+  applyAnswerDataList: ApplyAnswerTempData[];
+  fileUri: string;
+  portfolioUrl: string;
+  specialAnswerList: SpecialQuestionList;
+}
