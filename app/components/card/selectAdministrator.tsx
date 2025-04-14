cc27c2b8710046dbcda684c86b0fed4104cb81b1
bsu0404 <bsu0404@naver.com>
2025-04-14 12:25:14 +0900

    feat: Implement club member search and delegation functionality

--- app/components/card/selectAdministrator.tsx
+++ app/components/card/selectAdministrator.tsx
@@ -2,12 +2,21 @@
 
 import Image from "next/image";
 import SubPullDown from "../pulldown/subPullDown";
-import { Delegator, JoinedClub } from "@/types/components/delegate";
+import { Delegator } from "@/types/components/delegate";
+import { ClubData } from "@/types/club";
+import MemberSearch from "../input/memberSearch";
+import { useEffect, useState } from "react";
+import { entrustAdmin, getClubMembers } from "@/api/member/api";
+import { ClubMemberData, MemberData } from "@/types/member";
+
+import no_image from "@/images/noImage/no-image.jpg";
+import Alert from "../alert/alert";
+import { useUserStore } from "@/providers/userStoreProvider";
 
 interface SelectAdministratorProps {
-  club: JoinedClub;
-  selectedUser: Delegator;
-  setSelectedUser: (label: string, id: number) => void;
+  club: ClubData;
+  selectedUser: Delegator | null;
+  setSelectedUser: (label: string, id: string) => void;
 }
 
 const SelectAdministrator = ({
@@ -15,10 +24,39 @@ const SelectAdministrator = ({
   selectedUser,
   setSelectedUser,
 }: SelectAdministratorProps) => {
+  const { memberData } = useUserStore((state) => state);
+
+  const [nickname, setNickname] = useState<string>("");
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const [alertMessage, setAlertMessage] = useState<string | null>(null);
+
+  const isClubMemberData = (
+    member: MemberData | ClubMemberData
+  ): member is ClubMemberData => "clubMemberRoleType" in member;
+
+  const handleMemberClick = (member: ClubMemberData | MemberData) => {
+    if (!isClubMemberData(member)) {
+      setAlertMessage("잘못된 멤버 형식입니다.");
+      return;
+    }
+
+    if (member.memberData.memberId === memberData.memberId) {
+      setAlertMessage("본인에게 위임할 수 없습니다.");
+      return;
+    }
+    entrustAdmin(member.id)
+      .then(() => {
+        setAlertMessage(`${member.name}님에게 위임되었습니다.`);
+        setSelectedUser(member.name, member.id);
+      })
+      .catch(() => {
+        setAlertMessage("변경에 실패했습니다.");
+      });
+  };
   return (
     <div className="flex justify-start items-center px-2.5 py-2 gap-4 md:gap-3.5">
       <Image
-        src={club.image}
+        src={club.profileUri || no_image}
         alt={"club_image"}
         className="rounded-full object-cover w-12 h-12 md:w-[68px] md:h-[68px]"
       />
@@ -27,16 +65,30 @@ const SelectAdministrator = ({
           {club.name}
         </div>
         <div className="flex items-center gap-1 ">
-          <div className="text-subtext2 text-mobile_body1_m md:text-body2_m">
+          <div className="text-subtext2 text-mobile_body1_m md:text-body2_m whitespace-nowrap">
             위임대상 :
           </div>
-          <SubPullDown
-            optionData={club.users}
-            selectedOption={selectedUser.name}
-            handleOptionWithId={setSelectedUser}
-          />
+          {selectedUser ? (
+            <div className="text-mobile_body1_m md:text-body2_m">
+              {selectedUser.name}
+            </div>
+          ) : (
+            <MemberSearch
+              nickname={nickname}
+              setNickname={setNickname}
+              setErrorMessage={setErrorMessage}
+              placeholder={"멤버 검색"}
+              type="CLUB_MEMBER"
+              clubId={club.id}
+              handleMemberClick={handleMemberClick}
+            />
+          )}
         </div>
       </div>
+      {/* ====== 알림 ======*/}
+      {alertMessage && (
+        <Alert text={alertMessage} onClose={() => setAlertMessage(null)} />
+      )}
     </div>
   );
 };
