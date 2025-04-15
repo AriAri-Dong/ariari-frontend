8d628ff93144b21cb457a37743a828bf2b32cbf8
Seyoun <lsy20140@gmail.com>
2025-04-15 23:16:48 +0900

    refactor: Remove NotificationItemWrapper

--- app/components/list/notificationList.tsx
+++ app/components/list/notificationList.tsx
@@ -1,9 +1,9 @@
+import { useClubNotificationMutation } from "@/hooks/notification/useNotificationMutation";
 import rightArrow from "@/images/icon/vector.svg";
 import { NotificationData } from "@/types/notification";
 
 import formatDateToDot, { formatTime } from "@/utils/formatDateToDot";
 import Image from "next/image";
-import Link from "next/link";
 
 interface NotificationListProps {
   notificationList: NotificationData[];
@@ -15,6 +15,11 @@ const NotificationList = ({
   notificationList,
   className,
 }: NotificationListProps) => {
+  const { readClubNotification } = useClubNotificationMutation();
+  const handleClickNotification = (id: string) => {
+    readClubNotification.mutate({ clubAlarmId: id });
+  };
+
   if (!notificationList.length) {
     return <p className="text-center">새로운 알림이 없습니다.</p>;
   }
@@ -27,8 +32,13 @@ const NotificationList = ({
             key={id}
             className={`py-[14px] px-2.5 border-b last:border-b-0 ${className}`}
           >
-            <NotificationItemWrapper item={item}>
-              <div className="w-full flex flex-col gap-2 text-left md:gap-[6px]">
+            <button
+              className={`w-full flex items-center justify-between gap-3 ${
+                isChecked && "opacity-70"
+              }`}
+              onClick={() => handleClickNotification(id)}
+            >
+              <div className="flex flex-col gap-2 text-left md:gap-[6px]">
                 <h3
                   className={`text-mobile_body1_m md:text-body2_m ${
                     isChecked ? "text-subtext2" : "text-text1"
@@ -41,51 +51,16 @@ const NotificationList = ({
                   <p>{formatTime(new Date(createdDateTime))}</p>
                 </div>
               </div>
-            </NotificationItemWrapper>
+              <Image
+                src={rightArrow}
+                alt={"알림 바로가기"}
+                className="w-5 h-5 md:w-6 md:h-6"
+              />
+            </button>
           </li>
         );
       })}
     </ul>
-  );
-};
-
-// uri 존재 여부에 따른 상위 wrapper
-const NotificationItemWrapper = ({
-  item,
-  children,
-  className,
-}: {
-  item: NotificationData;
-  children: React.ReactNode;
-  className?: string;
-}) => {
-  const { uri, isChecked } = item;
-
-  const baseClassName = "w-full flex items-center justify-between gap-3";
-  const newClassName = `${baseClassName} ${className ?? ""} ${
-    isChecked ? "opacity-70" : ""
-  }`;
-
-  if (uri) {
-    return (
-      <Link href={uri} className={newClassName}>
-        {children}
-        <Image
-          src={rightArrow}
-          alt={"알림 바로가기"}
-          className="w-5 h-5 md:w-6 md:h-6"
-        />
-      </Link>
-    );
-  }
-
-  return (
-    <button
-      onClick={() => console.log("개별 알림 항목 클릭")}
-      className={newClassName}
-    >
-      {children}
-    </button>
   );
 };
 
