47a8b7f77bbd3d2ab149b59e4d454683d70226ca
Seyoun <lsy20140@gmail.com>
2025-04-23 01:24:39 +0900

    feat: Add deleteNotice mutation and modify hook

--- app/hooks/club/useClubNoticeMutation.tsx
+++ app/hooks/club/useClubNoticeMutation.tsx
@@ -1,24 +1,45 @@
-import { addClubNotice, AddClubNoticeParams } from "@/api/club/notice/api";
+import {
+  addClubNotice,
+  AddClubNoticeParams,
+  deleteClubNotice,
+} from "@/api/club/notice/api";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
-// 동아리 공지 등록
-export const useClubNoticeMutation = () => {
+export const useClubNoticeMutation = ({ clubId }: { clubId: string }) => {
   const queryClient = useQueryClient();
 
-  const mutation = useMutation({
-    mutationFn: ({ clubId, formData }: AddClubNoticeParams) =>
-      addClubNotice({ clubId, formData }),
-    onSuccess: (_, variables) => {
-      const { clubId } = variables;
+  // 공통 캐시 초기화 함수
+  const invalidateNoticeQueries = (clubId: string) => {
+    queryClient.invalidateQueries({
+      queryKey: ["club", clubId, "notices"],
+    });
+  };
 
-      queryClient.invalidateQueries({
-        queryKey: ["club", clubId, "notices"],
-      });
+  // 공지사항 등록
+  const addNotice = useMutation({
+    mutationFn: ({ formData }: AddClubNoticeParams) =>
+      addClubNotice({ clubId, formData }),
+    onSuccess: () => {
+      invalidateNoticeQueries(clubId);
     },
     onError: (error) => {
       console.log("add new club notice error", error);
     },
   });
 
-  return { addClubNotice: mutation };
+  // 공지사항 삭제
+  const deleteNotice = useMutation({
+    mutationFn: ({ clubNoticeId }: { clubNoticeId: string }) =>
+      deleteClubNotice(clubNoticeId),
+    onSuccess: () => {
+      invalidateNoticeQueries(clubId);
+    },
+    onError: (error) => {
+      console.log("delete club notice error", error);
+    },
+  });
+
+  return { addNotice, deleteNotice };
 };
+
+// 동아리 공지사항 삭제
