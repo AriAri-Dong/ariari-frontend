3bc6fed0099180a069ad089efca1c318351a106c
bsu0404 <bsu0404@naver.com>
2025-05-02 17:05:55 +0900

    feat: Create formatKSTTime util

---
+++ app/utils/formatKSTTime.ts
@@ -0,0 +1,21 @@
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+/**
+ *
+ * @param utcISOString  ex)"2025-05-01T06:00:00Z"
+ * @param format ex) "YYYY. MM. DD"
+ */
+
+export const formatKSTTime = (
+  utcISOString: string,
+  format = "YYYY-MM-DDTHH:mm:ss.SSSZ"
+): string => {
+  const kst = dayjs(utcISOString).add(9, "hour");
+
+  return kst.format(format);
+};
