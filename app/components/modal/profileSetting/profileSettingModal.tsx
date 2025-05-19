6fc079da660d8535d2d30d8874ddb716f0e14b7e
yeoniii20 <100350818+yeoniii20@users.noreply.github.com>
2025-05-19 18:14:13 +0900

    feat: Add window reload

--- app/components/modal/profileSetting/profileSettingModal.tsx
+++ app/components/modal/profileSetting/profileSettingModal.tsx
@@ -20,12 +20,16 @@ const ProfileSettingModal = ({ onClose }: LoginModalProps) => {
   const handleClose = () => {
     setStep(1);
     onClose();
+    window.location.reload();
   };
 
   return (
     <ProfileProvider>
       <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm">
-        <div className="absolute inset-0" onClick={handleClose} />
+        <div
+          className="absolute inset-0"
+          onClick={step === 4 ? handleClose : undefined}
+        />
         {step < 4 ? (
           <ProfileSetting step={step} onNextStep={handleNextStep} />
         ) : (
