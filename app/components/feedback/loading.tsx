68fd5b1d384899e1e1f200992925f7edb3461eb9
bsu0404 <bsu0404@naver.com>
2025-05-23 15:35:50 +0900

    refactor: Update loading component with new SVG

--- app/components/feedback/loading.tsx
+++ app/components/feedback/loading.tsx
@@ -11,21 +11,6 @@ interface LoadingProps {
 }
 
 const Loading = ({ className = "", lottieClassName = "" }: LoadingProps) => {
-  const [isForward, setIsForward] = useState<number>(1);
-  const lottieRef = useRef<any>(null);
-
-  // 재생, 역재생 반복
-  const handleComplete = () => {
-    setIsForward(-isForward);
-    lottieRef.current.setDirection(-isForward);
-  };
-
-  useEffect(() => {
-    if (lottieRef.current) {
-      lottieRef.current.play();
-    }
-  }, [isForward]);
-
   return (
     <div
       className={`flex flex-col items-center justify-center gap-3 min-h-[300px] md:min-h-[450px] text-center ${className}`}
@@ -35,8 +20,6 @@ const Loading = ({ className = "", lottieClassName = "" }: LoadingProps) => {
         loop={false}
         autoPlay
         className={`text-center w-20 h-20 ${lottieClassName}`}
-        lottieRef={lottieRef}
-        onComplete={handleComplete}
       />
       <p className="text-mobile_h4_r text-subtext1">Loading..</p>
     </div>
