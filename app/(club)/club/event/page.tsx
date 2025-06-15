36c04b60e65b96c6e9b547ff5452d7974a766390
yeoniii20 <commetoi_yeoni@naver.com>
2025-06-15 23:24:29 +0900

    fix: Modify SEO

--- app/(club)/club/event/page.tsx
+++ app/(club)/club/event/page.tsx
@@ -2,10 +2,10 @@ import MainSection from "./content/mainSection";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "일정 및 출석 | 활동 계획을 한눈에",
+  title: "일정 및 출석 | 아리아리",
   description: "활동 일정을 확인하고 출석을 간편하게 관리하세요.",
   openGraph: {
-    title: "일정 및 출석 | 활동 계획을 한눈에",
+    title: "일정 및 출석 | 아리아리",
     description: "활동 일정을 확인하고 출석을 간편하게 관리하세요.",
     url: "https://ariari.com/club/event",
     siteName: "아리아리",
--- app/(club)/club/management/recruitment/create/page.tsx
+++ app/(club)/club/management/recruitment/create/page.tsx
@@ -2,10 +2,10 @@ import { Metadata } from "next";
 import MainSection from "./content/mainSection";
 
 export const metadata: Metadata = {
-  title: "동아리 생성 | 새로운 동아리를 시작하세요",
+  title: "동아리 생성 | 아리아리",
   description: "새로운 동아리를 생성하고 활동을 시작할 수 있는 페이지입니다.",
   openGraph: {
-    title: "동아리 생성 | 새로운 동아리를 시작하세요",
+    title: "동아리 생성 | 아리아리",
     description: "새로운 동아리를 생성하고 활동을 시작할 수 있는 페이지입니다.",
     url: "https://ariari.com/club/management/recruitment/create",
     siteName: "아리아리",
