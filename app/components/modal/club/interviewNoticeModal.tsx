5a993fca218fdb78f4e2109481174abd8458dc10
Seyoun <lsy20140@gmail.com>
2025-07-11 22:45:24 +0900

    refactor: Replace placeholder with default value in interview notice modal textarea

--- app/components/modal/club/interviewNoticeModal.tsx
+++ app/components/modal/club/interviewNoticeModal.tsx
@@ -7,6 +7,15 @@ import SmallBtn from "@/components/button/basicBtn/smallBtn";
 import Contour from "@/components/bar/contour";
 import NotiPopUp from "../notiPopUp";
 
+const noticeDefaultValue = `안녕하세요.
+동아리 면접 대상자로 선정되신 것을 축하드리며, 면접 일정을 안내 드립니다.
+부득이한 사정으로 참석이 어려우시거나 면접 일시 변경이 필요하신 경우, 담당자에게 사전에 연락 부탁드립니다.
+    • 면접 일자 : 
+    •  면접 시간 : 
+    •  면접 장소 : 
+    •  담당자 연락처 : 
+감사합니다. 좋은 결과 있으시길 바랍니다.`;
+
 export interface InterviewNoticeModalProps {
   onClose: () => void;
   onSubmit: (message: string) => void;
@@ -16,7 +25,7 @@ const InterviewNoticeModal = ({
   onClose,
   onSubmit,
 }: InterviewNoticeModalProps) => {
-  const [details, setDetails] = useState<string>("");
+  const [details, setDetails] = useState<string>(noticeDefaultValue);
   const [alertMessage, setAlertMessage] = useState<string | null>(null);
   const [showConfirmModal, setShowConfirmModal] = useState<boolean>(false);
 
@@ -29,10 +38,11 @@ const InterviewNoticeModal = ({
   };
 
   const handleSubmit = () => {
-    if (validateForm()) {
-      onSubmit(details);
-      onClose();
-    }
+    console.log("datails", details);
+    // if (validateForm()) {
+    //   onSubmit(details);
+    //   onClose();
+    // }
   };
 
   const handleClose = () => {
@@ -85,14 +95,6 @@ const InterviewNoticeModal = ({
           <p className="text-noti text-body2_m">*</p>
         </h3>
         <textarea
-          placeholder={`안녕하세요.
-동아리 면접 대상자로 선정되신 것을 축하드리며, 면접 일정을 안내 드립니다.
-부득이한 사정으로 참석이 어려우시거나 면접 일시 변경이 필요하신 경우, 담당자에게 사전에 연락 부탁드립니다.
-    • 면접 일자 : 
-    •  면접 시간 : 
-    •  면접 장소 : 
-    •  담당자 연락처 : 
-감사합니다. 좋은 결과 있으시길 바랍니다.`}
           value={details}
           onChange={(e) => setDetails(e.target.value)}
           maxLength={1000}
