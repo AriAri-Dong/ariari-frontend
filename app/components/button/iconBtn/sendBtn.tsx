0d705cefd5d6cb9ffa0ac3fdc4115494af58f3fc
yeoniii20 <commetoi_yeoni@naver.com>
2025-07-16 23:49:59 +0900

    feat: Add disabled prop

--- app/components/button/iconBtn/sendBtn.tsx
+++ app/components/button/iconBtn/sendBtn.tsx
@@ -4,17 +4,29 @@ import Image from "next/image";
 import send from "@/images/icon/send.svg";
 import { ButtonProps } from "@/types/components/button";
 
-const SendBtn = ({ onClick }: ButtonProps) => {
+interface SendBtnProps extends ButtonProps {
+  disabled?: boolean;
+}
+
+const SendBtn = ({ onClick, disabled = false }: SendBtnProps) => {
   return (
     <button
-      className="flex justify-center items-center w-8 h-8 md:w-9 md:h-9
-    bg-primary active:bg-primary_hover md:hover:bg-primary_hover md:active:bg-primary_pressed
-    rounded-full  border border-menuborder cursor-pointer"
-      onClick={onClick}
+      type="button"
+      onClick={disabled ? undefined : onClick}
+      disabled={disabled}
+      className={`
+        flex justify-center items-center w-8 h-8 md:w-9 md:h-9
+        rounded-full border border-menuborder
+        ${disabled ? "bg-gray-200 opacity-50" : "bg-primary cursor-pointer"}
+        ${
+          !disabled &&
+          "active:bg-primary_hover md:hover:bg-primary_hover md:active:bg-primary_pressed"
+        }
+      `}
     >
       <Image
         src={send}
-        alt={"전송"}
+        alt="전송"
         width={20}
         height={20}
         className="md:w-6 md:h-6"
